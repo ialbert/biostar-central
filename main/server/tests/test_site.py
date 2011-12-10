fb3bedbe6eec4a8d754ea6c2262b46ead1b03faf
Istvan Albert <istvan.albert@gmail.com>
2011-12-09 23:20:28 -0500

    more refactoring

--- main/server/tests/test_site.py
+++ main/server/tests/test_site.py
@@ -15,26 +15,37 @@ class BiostarSiteTest(unittest.TestCase):
         true, eq = self.assertTrue, self.assertEqual
         c = Client()
         
+        # make a new user
+        user1, flag1 = User.objects.get_or_create(first_name='John', last_name='Doe', username='john', email='john')
+        user1.set_password('test')
+        user1.save()
+
         locs = "/ /about/ /user/list/ /tag/list/ /badge/list/ /question/unanswered/".split()
         for loc in locs:
             r = c.get(loc)
             eq(r.status_code, 200)
         
+        r = c.get("/user/list/")
+        eq(r.status_code, 200)
+        true('Doe' in r.content)
+
         r = c.get("/new/question/")
         eq(r.status_code, 302)
 
-        # make a new user
-        user1, flag1 = User.objects.get_or_create(first_name='John', last_name='Doe', username='john', email='john')
-        user1.set_password('test')
-        user1.save()
-        
         c.login(username='john', password='test')
         r = c.get("/new/question/")
         eq(r.status_code, 200)
         
         # post a question
-        r = c.post("/new/question/", {'title': 'My new question', 'content': 'this is the content of my question', 'tag_val':'aaa bbb ccc'})
+        title   = 'My new question about XYZBC123'
+        content = 'This is the content of my question!'
+        r = c.post("/new/question/", {'title':title , 'content':content , 'tag_val':'aaa bbb ccc', 'type':POST_QUESTION})
+        eq(r.status_code, 302)
+
+        r = c.get("/")
         eq(r.status_code, 200)
+        true(title in r.content)
+      
 
 def suite():
     
