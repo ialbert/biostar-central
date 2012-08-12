fa2a6de066c809068e92ec8ed89ce232c15e235c
Istvan Albert <istvan.albert@gmail.com>
2012-08-12 11:13:57 -0400

    tuning the shell scripts for data migration automation

--- main/bin/extract.py
+++ main/bin/extract.py
@@ -4,20 +4,23 @@ Extracts parameters for the biostar.sh script
 import sys
 from django.conf import settings
 
+
 if __name__ == '__main__':
+    
     if len(sys.argv)!= 2:
-        sys.exit('(!) one value may be extracted at a time' )
+        print ('(!) one value may be extracted at a time' )
+        sys.exit()
 
     key = sys.argv[1]
     engine = settings.DATABASES['default']['ENGINE']
     is_postgres = engine.endswith('psycopg2')
-     
+
     if is_postgres:
         remap = dict(PG_DBNAME="NAME", PG_USER="USER", PG_PASSWD="PASSWORD")
     else:
         remap = dict(SQLITE_DBNAME="NAME")
     
     key = remap.get(key, '')
-    value =  settings.DATABASES['default'].get(key, '<not set>')
+    value =  settings.DATABASES['default'].get(key, '')
     
     print value
