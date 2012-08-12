d67b9c25892df0bca15623cd72edde9903be67b3
Istvan Albert <istvan.albert@gmail.com>
2012-08-12 10:21:23 -0400

    more customization to data migration

--- main/bin/data-migration.sh
+++ main/bin/data-migration.sh
@@ -13,13 +13,27 @@
 #
 set -ue
 
+#
+# make a copy of this file and customize as necessary
+#
+source conf/default
+
+export PG_DBNAME=biostar-database
+export PG_USER=biostar-user
+export PG_PASSWD=biostar-password
+
 PGDUMP=updated-biostar.sql
 
 START_REV=0938eac
 START_ENV=migconf/migrate_start.env
 
+# this is currently ignored and the latest 
+# revision is taken
 END_REV=de9a834
 END_ENV=migconf/migrate_end.env
+
+#this is only necessary if the migration is repeated
+#git branch -D start-migration
 
 # check out the schema before the database migration
 git checkout -b start-migration $START_REV
@@ -33,6 +47,7 @@ source $START_ENV
 # get the most up to date version
 git checkout master
 
+# apply the new settings
 source libs/$END_ENV
 
 python manage.py syncdb
