81930c8258e186b0940b9b857de176c05dc1690c
Istvan Albert <istvan.albert@gmail.com>
2012-08-14 10:38:08 -0400

    added user modification from command line

--- main/bin/usermod.py
+++ main/bin/usermod.py
@@ -11,13 +11,20 @@ def usermod(uid, role):
     user = models.User.objects.get(pk=uid)
     user.profile.type = role
     user.profile.save()
+    if role == USER_ADMIN:
+        user.is_staff = user.is_superuser = True
+    else:
+        user.is_staff = user.is_superuser = False
+        
+    user.save()
+        
     
 if __name__ == '__main__':    
     import optparse
     usage = "usage: %prog [options] file1 file2 ..."
     parser = optparse.OptionParser(usage=usage)
     parser.add_option("-u", "--userid", dest="uid", type=int, help="The ID of the user", default=0)
-    parser.add_option("-r", "--role", dest="role", type=int, help="The role for this user", default=0)
+    parser.add_option("-r", "--role", dest="role", type=int, help="The role for this user (as integer)", default=0)
     
     (opts, args) = parser.parse_args()
     
