c0fe86102635ea76f132a994ccd2706e97c88b06
Istvan Albert <istvan.albert@gmail.com>
2012-11-21 20:11:19 -0500

    updated cron jobs

--- main/bin/cron/daily.sh
+++ main/bin/cron/daily.sh
@@ -4,6 +4,9 @@ source /home/biostar/www/production/live/biostar.env
 
 set -ue
 
+# remove old files
+find /home/biostar/data/*.gz -type f -mtime +3 -exec rm {} \;
+
 $PYTHON_EXE -m main.bin.sitemap
 $PYTHON_EXE -m main.bin.planet --download
 $PYTHON_EXE -m main.bin.patch --reduce_notes -n 6
