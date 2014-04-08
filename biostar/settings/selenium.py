ec7a50908205afd030947d7e02886772c50644a8
Istvan Albert <istvan.albert@gmail.com>
2014-04-08 14:57:05 -0400

    adding settings file for selenium

---
+++ biostar/settings/selenium.py
@@ -0,0 +1,16 @@
+# -*- coding: utf8 -*-
+#
+# Django settings for selenium testing
+#
+from biostar.settings.base import *
+
+# Turn off captcha
+CAPTCHA = False
+
+# How many non top level posts per day for users.
+MAX_POSTS_NEW_USER = 100
+MAX_POSTS_TRUSTED_USER = 100
+
+# How many top level posts per day for a new user.
+MAX_TOP_POSTS_NEW_USER = 100
+MAX_TOP_POSTS_TRUSTED_USER = 100
