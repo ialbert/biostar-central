517613a3440cd4bdca27ae07499e5300b598e505
Istvan Albert <istvan.albert@gmail.com>
2014-04-09 14:22:38 -0400

    similar post cache bugfix

--- biostar/settings/selenium.py
+++ biostar/settings/selenium.py
@@ -14,3 +14,10 @@ MAX_POSTS_TRUSTED_USER = 100
 # How many top level posts per day for a new user.
 MAX_TOP_POSTS_NEW_USER = 100
 MAX_TOP_POSTS_TRUSTED_USER = 100
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake'
+    }
+}
