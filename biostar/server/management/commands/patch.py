dfcf6e6547f43e9dc7bf704bf5a86fa53d16451b
Istvan Albert <istvan.albert@gmail.com>
2014-04-10 20:38:05 -0400

    updates

---
+++ biostar/server/management/commands/patch.py
@@ -0,0 +1,31 @@
+__author__ = 'ialbert'
+
+from django.core.management import call_command
+from django.conf import settings
+from django.db import connection, transaction
+from django.db.models.loading import get_app
+from StringIO import StringIO
+from django.core.management.base import BaseCommand, CommandError
+import os
+from optparse import make_option
+
+class Command(BaseCommand):
+    help = 'Runs quick patches over the data'
+
+    option_list = BaseCommand.option_list + (
+        make_option('--users', dest='users', action='store_true', default=False, help='patches_users'),
+    )
+
+    def handle(self, *args, **options):
+
+        if options['users']:
+            patch_users()
+
+def patch_users():
+    from biostar.apps.users.models import User, Profile
+    from biostar.const import EMAIL_MESSAGE
+
+    users = Profile.objects.all()
+    users.update(message_prefs=EMAIL_MESSAGE)
+
+
