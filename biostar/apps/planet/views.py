b52831c35d84842b7e19c17c4c6b0b8b5d23f2b3
Istvan Albert <istvan.albert@gmail.com>
2014-04-25 15:10:11 -0400

    bugfix for planet count not going to zero

--- biostar/apps/planet/views.py
+++ biostar/apps/planet/views.py
@@ -1,7 +1,15 @@
 # Create your views here.
 from django.views.generic import DetailView, ListView, TemplateView, UpdateView, View
 from .models import Blog, BlogPost
+from django.conf import settings
 
+def reset_counts(request, label):
+    "Resets counts in the session"
+    label = label.lower()
+    counts = request.session.get(settings.SESSION_KEY, {})
+    if label in counts:
+        counts[label] = ''
+        request.session[settings.SESSION_KEY] = counts
 
 class BlogPostList(ListView):
     template_name = "planet/planet_entries.html"
@@ -15,11 +23,13 @@ class BlogPostList(ListView):
 
     def get_context_data(self, **kwargs):
         get = self.request.GET.get
+        self.topic = 'planet'
         context = super(BlogPostList, self).get_context_data(**kwargs)
         context['page_title'] = "Planet"
-        context['topic'] = 'planet'
+        context['topic'] = self.topic
         context['limit'] = get('limit', '')
         context['q'] = get('q', '')
         context['sort'] = get('sort', '')
         context['blogs'] = Blog.objects.all().order_by("-list_order")
+        reset_counts(self.request, self.topic)
         return context
