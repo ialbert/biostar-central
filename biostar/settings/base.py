5f87569d34dd35defbd1287ed6875a514229b975
Istvan Albert <istvan.albert@gmail.com>
2014-05-14 09:54:13 -0400

    added width/height to allowed html attributes

--- biostar/settings/base.py
+++ biostar/settings/base.py
@@ -127,11 +127,11 @@ CONN_MAX_AGE = 10;
 
 # Allowed html content.
 ALLOWED_TAGS = "p div br code pre h1 h2 h3 h4 hr span s sub sup b i img strong strike em underline super table thead tr th td tbody".split()
-ALLOWED_STYLES = 'color font-weight background-color'.split()
+ALLOWED_STYLES = 'color font-weight background-color width height'.split()
 ALLOWED_ATTRIBUTES = {
     '*': ['class', 'style'],
     'a': ['href', 'rel'],
-    'img': ['src', 'alt'],
+    'img': ['src', 'alt', 'width', 'height'],
     'table': ['border', 'cellpadding', 'cellspacing'],
 }
 
