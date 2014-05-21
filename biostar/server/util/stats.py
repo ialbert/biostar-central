568ace5624d49395ce716b6b2205cbdd92c39ac1
nimiq <puntonim@gmail.com>
2014-05-21 18:14:17 +0200

    Add cached stats files for /api/stats/<days>

--- biostar/server/util/stats.py
+++ biostar/server/util/stats.py
@@ -2,10 +2,23 @@
 Utility module for statistics about the website.
 """
 from datetime import datetime, timedelta
+import logging
+import time
+from os.path import join, normpath, isfile, exists
+from os import makedirs
+import json
+
+from django.conf import settings
 
 from biostar.apps.posts.models import PostView, Post, Vote
 from biostar.apps.users.models import User
 from .dates import datetime_to_iso, datetime_to_unix
+
+
+logger = logging.getLogger(__name__)
+
+
+STATS_FOLDER = normpath(join(settings.EXPORT_DIR, 'stats'))
 
 
 def count_traffic(start, end):
@@ -25,6 +38,7 @@ def count_traffic(start, end):
 def compute_stats(days_ago=0):
     """
     Statistics about this website for the given day.
+    Statistics are stored to a json file for caching purpose.
 
     Parameters:
     days_ago -- a day, given as a number of days ago.
@@ -33,6 +47,11 @@ def compute_stats(days_ago=0):
     now = datetime.now().date()
     start = now - timedelta(days=days_ago)
     end = start + timedelta(days=1)
+
+    try:
+        return load_stats_from_file(start)
+    except IOError:  # This will be FileNotFoundError in Python3.
+        logger.info('No stats file for {}.'.format(start))
 
     query = Post.objects.filter
 
@@ -67,4 +86,50 @@ def compute_stats(days_ago=0):
         'new_posts': new_posts_ids,
         'new_votes': new_votes_ids,
     }
+    dump_stats_to_file(start, data)
     return data
+
+
+def load_stats_from_file(date):
+    """
+    Load stats data from a stat file.
+
+    Params:
+    date -- a `datetime` instance.
+    """
+    file_path = _build_stats_file_path(date)
+
+    if not isfile(file_path):
+        raise IOError  # This will be FileNotFoundError in Python3.
+
+    with open(file_path, 'r') as fin:
+        return json.loads(fin.read())
+
+
+def dump_stats_to_file(date, data):
+    """
+    Store stats data to a json file for caching purpose.
+
+    Params:
+    date -- a `datetime` instance.
+    data -- stats data in a dictionary.
+    """
+    if not exists(STATS_FOLDER):
+        # Ensure STATS_FOLDER exists.
+        makedirs(STATS_FOLDER)
+
+    file_path = _build_stats_file_path(date)
+
+    with open(file_path, 'w') as fout:
+        fout.write(json.dumps(data))
+
+
+def _build_stats_file_path(date):
+    """
+    Build the path of a stat file from a date.
+
+    Params:
+    date -- a `datetime` instance.
+    """
+    file_name = time.strftime("%Y-%m-%d.json", date.timetuple())
+    return normpath(join(STATS_FOLDER, file_name))
