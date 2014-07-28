aa403d605d9099b0181005e315112f432ff0e5a5
nimiq <puntonim@gmail.com>
2014-07-28 23:05:40 +0200

    Simplify query to get bio
    There is actually no need to run such query because it has already been run by django-allauth.

--- biostar/server/orcid.py
+++ biostar/server/orcid.py
@@ -1,31 +1,8 @@
 import json
 
-from requests_oauthlib import OAuth2Session
-from allauth.socialaccount.models import SocialApp
-
 
 def import_orcid_profile_data(**kwargs):
-    provider = kwargs['sociallogin'].account.provider
-
-    if not 'orcid' in provider.lower():
-        return
-
-    social_token = kwargs['sociallogin'].token
-    uid = kwargs['sociallogin'].account.uid
-    data = _run_profile_query(provider, social_token.token, uid)
-
+    # The ORCID bio for a user was stored in the `extra_data` of his `SocialAccount` when he added
+    # ORCID as social login.
+    data = kwargs['sociallogin'].account.extra_data
     print(json.dumps(data, indent=4))
-
-
-def _run_profile_query(provider, token, uid):
-    oauth_sex = _create_orcid_oauth_sex(provider, token)
-    resource_url = 'https://api.sandbox.orcid.org/v1.1/{}/orcid-profile'.format(uid)
-    r = oauth_sex.get(resource_url, headers={'accept': 'application/orcid+json'})
-    return r.json()
-
-
-def _create_orcid_oauth_sex(provider, token):
-    orcid_provider = SocialApp.objects.get(provider=provider)
-    sex = OAuth2Session(client_id=orcid_provider.client_id,
-                        token={'access_token': token,})
-    return sex
