411507226180344c11fa0640d3571159bacea9d0
Istvan Albert <istvan.albert@gmail.com>
2015-02-03 15:59:21 -0500

    work on permissions for biostar 3.0

--- biostar3/forum/tests/test_objects.py
+++ biostar3/forum/tests/test_objects.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, division, unicode_literals
 
 from django.test import TestCase
 from django.conf import settings
@@ -6,16 +6,31 @@ from django.conf import settings
 from biostar3.forum import apps
 from biostar3.forum.models import User
 
-class SimpleTests(TestCase):
+from faker import Factory
 
+
+AUTH_FUNCS = [
+    apps.authorize_post_mod, apps.authorize_user_ban, apps.authorize_user_mod
+]
+
+
+class SimpleTests(TestCase):
     def test_admin_user(self):
         """
-        Admin user must exists and has the right permissions.
+        Test admin user creation.
         """
         for user, email in settings.ADMINS:
             user = User.objects.get(email=email)
-            funcs = [
-                apps.authorize_post_mod, apps.authorize_user_ban, apps.authorize_user_mod
-            ]
-            for func in funcs:
+
+            for func in AUTH_FUNCS:
                 self.assertEqual(func(user), True)
+
+    def test_user_gen(self):
+        """
+        Test normal user creation
+        """
+        f = Factory.create()
+        for i in range(10):
+            user = User.objects.create(name=f.name(), email=f.email())
+            for func in AUTH_FUNCS:
+                self.assertEqual(func(user), False)
