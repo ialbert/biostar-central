3ffa821dd9dcb368f228bb42c0c87acf5dcbb30d
Istvan Albert <istvan.albert@gmail.com>
2015-02-11 16:55:29 -0500

    work on biostar3

---
+++ run/devel.py
@@ -0,0 +1,11 @@
+from run.sqlite import *
+
+# add debugging middleware
+MIDDLEWARE_CLASSES = list(MIDDLEWARE_CLASSES)
+MIDDLEWARE_CLASSES.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
+
+# We load the debug toolbar as well
+INSTALLED_APPS = list(INSTALLED_APPS)
+
+# This needs to be added before the user models.
+INSTALLED_APPS.append( "debug_toolbar")
