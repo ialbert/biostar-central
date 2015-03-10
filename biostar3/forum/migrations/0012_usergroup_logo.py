cb80f9f3e59b03bb2d73cab509499ab115856e90
Istvan Albert <istvan.albert@gmail.com>
2015-03-10 11:51:35 -0400

    groups may upload their own logo for 3.0

---
+++ biostar3/forum/migrations/0012_usergroup_logo.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('forum', '0011_groups'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='usergroup',
+            name='logo',
+            field=models.FileField(null=True, upload_to='group_logo', blank=True),
+            preserve_default=True,
+        ),
+    ]
