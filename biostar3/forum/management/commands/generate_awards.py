6463239d37c21ef960d7a3a9906f24e6dfdc373b
Istvan Albert <istvan.albert@gmail.com>
2015-03-18 14:15:08 -0400

    award generation management script

--- biostar3/forum/management/commands/generate_awards.py
+++ biostar3/forum/management/commands/generate_awards.py
@@ -6,13 +6,14 @@ import os, logging, glob, json
 import html2text
 from biostar3.forum.models import Post
 from biostar3.forum.html import sanitize
+import time
 
 """
 Saves html to markdown format.
 """
 
 logger = logging.getLogger('biostar')
-from biostar3.forum import awards
+from biostar3.forum import awards, models
 
 def abspath(*args):
     "Generates absolute paths."
@@ -33,4 +34,9 @@ class Command(BaseCommand):
 
             for award in awards.get_awards():
                 logger.info("award %s" % award.uuid)
+                start = time.time()
                 award.check()
+                end = time.time()
+                at = int(end - start)
+                ac = models.Award.objects.filter(badge__uuid=award.uuid).count()
+                logger.info("%s %s awards, took %d seconds" % (ac, award.uuid, at))
