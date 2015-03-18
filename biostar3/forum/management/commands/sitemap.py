55e26b2c83cec966417a9bd3a4b59d45449f4b3c
Istvan Albert <istvan.albert@gmail.com>
2015-03-18 19:56:46 -0400

    added sitemap generator to 3.0

---
+++ biostar3/forum/management/commands/sitemap.py
@@ -0,0 +1,46 @@
+"""
+Creates a sitemap in the EXPORT directory
+"""
+import os
+from django.conf import settings
+from django.contrib.sitemaps import GenericSitemap
+from django.contrib.sites.models import Site
+from biostar3.forum.models import Post
+from django.utils.encoding import smart_str
+from django.template import loader
+from django.core.management.base import BaseCommand, CommandError
+import logging
+from django.contrib import sitemaps
+
+logger = logging.getLogger("command")
+
+def path(*args):
+    "Generates absolute paths"
+    return os.path.abspath(os.path.join(*args))
+
+
+class Command(BaseCommand):
+    help = 'Creates a sitemap in the export folder of the site'
+
+    def handle(self, *args, **options):
+        generate_sitemap()
+
+def generate_sitemap():
+    sitemap = GenericSitemap({
+        'queryset': Post.objects.filter(type__in=Post.TOP_LEVEL,
+                                        usergroup__public=True).order_by("creation_date")
+    })
+    urlset = sitemap.get_urls()
+    text = loader.render_to_string('sitemap.xml', {'urlset': urlset})
+    text = smart_str(text)
+    site = Site.objects.get_current()
+    fname = path(settings.STATIC_ROOT, 'sitemap.xml')
+
+    logger.info('writing sitemap for %s to %s' % (site, fname))
+    fp = open(fname, 'wt')
+    fp.write(text)
+    fp.close()
+
+if __name__ == '__main__':
+    #generate_sitemap()
+    pass
