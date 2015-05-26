913e798aa6caf0b1e1a85d9b954a5ee42907f3b9
Lohit13 <lohitaksh14059@iiitd.ac.in>
2015-05-27 04:44:39 +0530

    Edited phpbb script to not user external modules

--- biostar/server/management/commands/import_phpbb.py
+++ biostar/server/management/commands/import_phpbb.py
@@ -1,10 +1,10 @@
 from django.core.management.base import BaseCommand, CommandError
 from optparse import make_option
 import sys, logging, os
-import MySQLdb as mdb 
 from django.core.exceptions import ImproperlyConfigured
 from datetime import date
 from django.utils import timezone
+from django.conf import settings
 
 logger = logging.getLogger('simple-logger')
 
@@ -13,25 +13,60 @@ class Command(BaseCommand):
 
     option_list = BaseCommand.option_list + (
         make_option("-f", '--file', dest='file', default=False, help='import file'),
-        make_option("-l", '--host', dest='host', default='localhost', help='mysql hostname'),
-        make_option("-u", '--user', dest='user', default='root', help='mysql user'),
-        make_option("-p", '--pass', dest='pass', default=None, help='mysql password (will not be saved)'),
     )
 
     def handle(self, *args, **options):
         fname = options['file']
-        host = options['host']
-        user = options['user']
-        password = options['pass']
 
-        if fname and password:
-            import_sql_file(fname, host, user, password)
+        if fname:
+        	if fname.endswith('.sql'):
+        		import_bb_file(fname)
+        	else:
+        		logger.error('Wrong format! Please provide and sql file (.sql)')
         else:
         	if not fname:
         		logger.error('No file name supplied')
-        	if not password:
-        		logger.error('MySQL password needed')
         	logger.info('try -h for more help')
+
+
+#Removes unwanted lines from sql file
+def sanitize(filename):
+    f = open(filename,'r')
+    sqltemp=[]
+    for i in f.readlines():
+        if i[0]!='-' and i[0]!='/' and i[0]!='\n':
+            if i[-1:] == '\n':
+                i=i[:-1]
+            sqltemp.append(i)
+    sql=[]
+    temp=[]
+    for i in sqltemp:
+        if i[-1:]==';':
+            temp.append(i)
+            temp = ''.join(temp)
+            sql.append(temp)
+            temp=[]
+        else:
+            temp.append(i)
+    return sql
+
+
+#Returns the post title and body from the INSERT instance
+def spliting(string):
+    lt = []
+    for i in range(len(string)-1):
+        if string[i]=="," and string[i+1]=="'":
+            start=i+2
+            for j in range(i+1,len(string)-1):
+                if string[j]=="'" and string[j+1]==",":
+                    end=j
+                    break
+            temp=string[start:end]
+            lt.append(temp)
+            i=j+1
+
+    tb=[lt[2],lt[3]]
+    return tb
 
 
 #Fetches the environment variable
@@ -47,54 +82,8 @@ def get_env(name, func=None):
         raise ImproperlyConfigured(msg)
 
 
-#Removes unwanted lines from sql file
-def sanitize(filename):
-	f = open(filename,'r')
-	sqltemp=[]
-	for i in f.readlines():
-	    if i[0]!='-' and i[0]!='/' and i[0]!='\n':
-	    	if i[-1:] == '\n':
-	    		i=i[:-1]
-	        sqltemp.append(i)
-	sql=[]
-	temp=[]
-	for i in sqltemp:
-		if i[-1:]==';':
-			temp.append(i)
-			temp = ''.join(temp)
-			sql.append(temp)
-			temp=[]
-		else:
-			temp.append(i)
-	return sql
-
-
-#Creates/Delets the temporary mysql database
-def temp_db(host, user, password, cmd):
-	if cmd == 'c':
-		sql = 'CREATE DATABASE import_temp'
-	if cmd == 'd':
-		sql = 'DROP DATABASE import_temp'
-	conn = mdb.connect(host,user,password)
-	cursor = conn.cursor()
-	cursor.execute(sql)
-	conn.commit()
-	conn.close()
-
-
-#Imports the sql file to the temporary database
-def import_to_temp(sql, host, user, password):
-	conn = mdb.connect(host,user,password,'import_temp')
-	cursor = conn.cursor()
-	cursor._defer_warnings = True
-	for i in sql:
-		cursor.execute(i)
-	conn.commit()
-	conn.close()
-
-
 #Imports the posts into django models
-def import_posts(host, user, password):
+def import_posts(allposts):
 	from biostar.apps.users.models import User
 	from biostar.apps.posts.models import Post
 
@@ -109,21 +98,10 @@ def import_posts(host, user, password):
 		u.profile.last_login = timezone.now()
 		u.profile.save()
 
-	conn = mdb.connect(host,user,password,'import_temp')
-	cursor = conn.cursor()
-	cursor._defer_warnings = True
-	sql = 'select * from phpbb_posts;'
-	try:
-		cursor.execute(sql)
-		results = cursor.fetchall()
-	except:
-		logger.error('Unable to fetch posts from temp_db')
-		sys.exit()
-
 	post_count=0
-	for result in results:
-		title = result[14]
-		body = result[15]
+	for single in allposts:
+		title = single[0]
+		body = single[1]
 		logger.info('Fetched post : %s' % title)
 		if title.startswith('Re:'):
 			ptitle = title[4:]
@@ -142,25 +120,36 @@ def import_posts(host, user, password):
 			post_count+=1
 	logger.info('%d posts created' % post_count)
 
-	conn.close()
 
 
-#Governs the import process
-def import_sql_file(filename, host, user, password):
-	temp_db(host, user, password, 'c')
-	logger.info('Created temporary database')
+#Governs the improt process
+def import_bb_file(fname):
+	logger.info('Sanitizing file')
+	sql = sanitize(fname)
 
-	logger.info('Sanitizing input file')
-	sql = sanitize(filename)
+    #Retrieves index line with post table
+	logger.info('Parsing file...')
+	for i in range(0,len(sql)):
+		if sql[i].startswith('INSERT INTO `phpbb_posts`'):
+			index = i
+			break
 
-	logger.info('Importing to temp db...')
-	import_to_temp(sql, host, user, password)
+	sql = sql[index]
+	#Retrieves all posts
+	sql = sql[33:]
 
-	logger.info('Importing posts...')
-	import_posts(host, user, password)
+	#Split posts into different INSERT instances
+	sql = sql.split('),(')
 
-	temp_db(host, user, password, 'd')
-	logger.info('Deleted temporary database')
+	#Gettings all post title and body as list
+	logger.info('Retrieving posts...')
+	posts=[]
+	for i in sql:
+		post=spliting(i)
+		posts.append(post)
+
+	import_posts(posts)
+	logger.info('DONE!')
 
 
 if __name__ == '__main__':
