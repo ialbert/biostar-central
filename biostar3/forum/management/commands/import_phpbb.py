2c9ac10847a267f16bbfb2a882b6423acef1885f
Lohit13 <lohitaksh14059@iiitd.ac.in>
2015-05-30 04:46:28 +0530

    Modified phpbb script. Saves user info

--- biostar3/forum/management/commands/import_phpbb.py
+++ biostar3/forum/management/commands/import_phpbb.py
@@ -12,82 +12,147 @@ import csv
 logger = logging.getLogger('simple-logger')
 
 class Command(BaseCommand):
-    help = 'Import from .sql backup'
+    help = 'Import phpbb posts from csv backup'
 
     option_list = BaseCommand.option_list + (
-        make_option("-f", '--file', dest='file', default=False, help='import file'),
+        make_option("-p", '--post', dest='post', default=False, help='posts file'),
+        make_option("-u", '--user', dest='user', default=False, help='users file'),
     )
 
     def handle(self, *args, **options):
-        fname = options['file']
+        fname = options['post']
+        uname = options['user']
 
-        if fname:
-        	if fname.endswith('.csv'):
-        		import_bb_file(fname)
+        if fname and uname:
+        	if fname.endswith('.csv') and uname.endswith('.csv'):
+        		import_posts(fname,uname)
         	else:
         		logger.error('Wrong format! Please provide a csv file (.csv)')
         else:
         	if not fname:
-        		logger.error('No file name supplied')
+        		logger.error('No posts file name supplied')
+        	if not uname:
+        		logger.error('No users file name supplied')
         	logger.info('try -h for more help')
 
 
-#Extracts and returns posts from the csv
-def extract(fname):
-	f = open(fname, 'rb')
+#Extracts and returns all the posts
+def get_posts(fname):
+	f = open(fname,'rb')
 	rows = csv.reader(f)
+	rows.next()
 
 	allposts = []
 	for row in rows:
+		uid = row[3]
 		title = row[14]
 		body = row[15]
-		temp=[title,body]
+		temp = [uid,title,body]
 		allposts.append(temp)
 
-	allposts.pop(0)
+	f.close()
 	return allposts
 
 
+#Returns all users from file
+def get_all_users(uname):
+	f = open(uname,'rb')
+	rows = csv.reader(f)
+	rows.next()
+
+	allusers = []
+	for row in rows:
+		uid = int(row[0])
+		name = row[7]
+		email = row[12]
+		temp = [uid,name,email]
+		allusers.append(temp)
+
+	f.close()
+	return allusers 	
+
+
+#Extracts user information with user_id
+def get_user(uid, allusers):
+
+	found=0
+	for row in allusers:
+		if row[0] == uid:
+			name = row[1]
+			email = row[2]
+			found=1
+			break
+
+	try:
+		user = User.objects.get(email=email)
+		logger.info('Fetched user: %s' % user.name)
+	except:
+		user = User(email=email, name=name)
+		user.save()
+		logger.info('Created user: %s' % user.name)
+
+	return user 
+
+
 #Imports the posts into django models
-def import_posts(allposts):
+def import_posts(fname,uname):
 
-    # Get the default group.
-    default_group = UserGroup.objects.get(domain=settings.DEFAULT_GROUP_DOMAIN)
-
-    # Get the first admin user
-    admin = User.objects.get(email=settings.ADMINS[0][1])
-
-    post_count=0
-    for single in allposts:
-        title = single[0]
-        body = single[1]
-        logger.info('Fetched post : %s' % title)
-        if title.startswith('Re:'):
-            ptitle = title[4:]
-            try:
-                parent = Post.objects.get(title=ptitle)
-                post = Post(title=title, author=admin, type= Post.ANSWER, content=body, usergroup=default_group)
-                post.parent=parent
-                post.root=parent
-                post.save()
-                post_count+=1
-            except:
-                pass
-        else:
-            post = Post(title=title, author=admin, type = Post.QUESTION, content=body, usergroup=default_group)
-            post.save()
-            post_count+=1
-    logger.info('%d posts created' % post_count)    
-
-
-#Governs the improt process
-def import_bb_file(fname):
 	logger.info('Extracting posts from file...')
-	allposts = extract(fname)
+	allposts = get_posts(fname)
 
-	logger.info('Importing posts...')
-	import_posts(allposts)
-	logger.info('DONE!')
+	logger.info('Extracting users from file...')
+	allusers = get_all_users(uname)
+
+	# Get the default group.
+	default_group = UserGroup.objects.get(domain=settings.DEFAULT_GROUP_DOMAIN)
+
+	post_count=0
+	for single in allposts:
+		uid = int(single[0])
+		title = single[1]
+		body = single[2]
+		logger.info('Fetched post : %s' % title)
+
+		#Getting the post user
+		user = get_user(uid,allusers)
+
+		#List to hold answer posts which could not be matched
+		orphans = []
+
+		if title.startswith('Re:'):
+			try:
+				ptitle = title[4:]
+				parent = Post.objects.get(title__startswith=ptitle)
+				post = Post(title=title, author=user, type= Post.ANSWER, content=body, usergroup=default_group)
+				post.parent=parent
+				post.root=parent
+				post.save()
+				post_count+=1
+			except:
+				post = Post(title=title, author=user, type= Post.ANSWER, content=body, usergroup=default_group)
+				orphans.append(post)
+		else:	
+			post = Post(title=title, author=user, type = Post.QUESTION, content=body, usergroup=default_group)
+			post.save()
+			post_count+=1
+
+	#Now try to match posts which could not be matched before
+	if orphans:
+		logger.info('Matching posts which could not be matched earlier')
+		for post in orphans:
+			try:
+				title = post.title
+				ptitle = title[4:]
+				parent = Post.objects.get(title__startswith=ptitle)
+				post.parent=parent
+				post.root=parent
+				post.save()
+				post_count+=1
+			except:
+				pass
+	
+	logger.info('%d posts created' % post_count)   
+	logger.info('DONE!') 
 
 
 if __name__ == '__main__':
