a8aa3a8b4c6538753e43a0a0c3b27bbc2a26e2ee
Istvan Albert <istvan.albert@gmail.com>
2015-08-17 09:47:31 -0400

    removed files

--- live/deploy.py
+++
@@ -1,8 +0,0 @@
-from biostar.settings.base import *
-
-SITE_DOMAIN = "mysite.com"
-SERVER_EMAIL = "myemail@mysite.com"
-
-# Override any other settings that you might need
-# See the biostars/settings/base.py for settings options
-
