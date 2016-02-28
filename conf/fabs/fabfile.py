3876ee1088196429547c0d8df094defc29c81d2f
Istvan Albert <istvan.albert@gmail.com>
2016-02-28 18:01:38 -0500

    fabric file changes

--- conf/fabs/fabfile.py
+++ conf/fabs/fabfile.py
@@ -2,7 +2,50 @@ from fabric.context_managers import prefix
 from fabric.api import *
 from fabric.contrib.files import exists
 from getpass import getpass
-from sites import *
+
+BIOSTAR_HOME = "/home/www/sites/biostar-central"
+VIRTUALENV_WRAPPER = "source /usr/local/bin/virtualenvwrapper.sh"
+CLONE_URL = "https://github.com/ialbert/biostar-central.git"
+BRANCH = "master"
+
+def setenv():
+    # The python environment that the system needs.
+    env.biostar_home = BIOSTAR_HOME
+    env.wrapper = VIRTUALENV_WRAPPER
+    env.biostar_clone = CLONE_URL
+    env.biostar_branch = BRANCH
+
+    # The is the main environment that will be applied to each command.
+    # This is the prefix invoked when opertating on the deployed site.
+    env.biostar_live = "%(biostar_home)s/live" % env
+    env.biostar_env = "%(biostar_live)s/deploy.env" % env
+    env.workon = "source /usr/local/bin/virtualenvwrapper.sh && workon biostar && cd %(biostar_home)s && source %(biostar_env)s" % env
+
+def usegalaxy(user="www"):
+    "Sets the environment for the biostar galaxy"
+    setenv()
+    env.hosts.append('biostar.usegalaxy.org')
+    env.user = user
+
+def metastars(user="www"):
+    "Sets the environment for the biostar galaxy"
+    setenv()
+    env.hosts.append('metastars.org')
+    env.user = user
+
+def biostars(user="www"):
+    "Sets the environment for the biostar galaxy"
+    setenv()
+    env.hosts.append('biostars.org')
+    env.user = user
+
+def test_site(user='www'):
+    setenv()
+    env.hosts.append('test.biostars.org')
+    env.user = user
+
+def hostname():
+    run("hostname")
 
 def copy_config():
     # Create a default environment.
