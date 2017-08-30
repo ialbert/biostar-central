6f28d8787a720b1e763f724f87676397734522e0
Istvan Albert <istvan.albert@gmail.com>
2017-08-30 16:01:13 -0400

    Update by ialbert on Wed Aug 30 16:01:13 EDT 2017 from yolo.local

--- live/main_settings.py
+++ live/main_settings.py
@@ -1,3 +1,5 @@
 from engine.settings import *
 
 WSGI_APPLICATION = 'live.main_wsgi.application'
+
+ALLOWED_HOSTS = [ 'www.planktontow.com' ]
--- live/test_settings.py
+++ live/test_settings.py
@@ -1,3 +1,5 @@
 from engine.settings import *
 
 WSGI_APPLICATION = 'live.test_wsgi.application'
+
+ALLOWED_HOSTS = [ 'test.planktontow.com' ]
