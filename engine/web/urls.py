9a4ad0b9129ee9a6502345ba3107ce1aba1bd1d6
Natay <b.natay@gmail.com>
2017-09-04 15:36:47 -0400

    log in template works; now the backend

--- engine/web/urls.py
+++ engine/web/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name="home"),
+    url(r'^login/$', views.login, name="login"),
     url(r'^blog/$', views.blog, name="blog"),
     url(r'^data/$', views.data, name="data"),
     url(r'^forum/$', views.forum, name="forum"),
--- engine/web/views.py
+++ engine/web/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
 
+#TEMPERORY 
+def login(request):
+    return render (request, 'login.html')
+
+
 def index(request):
     return render(request, 'index.html')
 
