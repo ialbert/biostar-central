9db82305cb7cfad2a4ed96c51f9f647fe07dd631
Natay <b.natay@gmail.com>
2017-09-04 18:41:22 -0400

    added signup and login; adding personal profiles for uses

--- engine/web/urls.py
+++ engine/web/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name="home"),
     url(r'^login/$', views.login, name="login"),
-    url(r'^blog/$', views.blog, name="blog"),
-    url(r'^data/$', views.data, name="data"),
-    url(r'^forum/$', views.forum, name="forum"),
+    url(r'^signup/$', views.signup, name="signup"),
+    url(r'^logout/$', views.logout, name="logout"),
 ]
--- engine/web/views.py
+++ engine/web/views.py
@@ -1,19 +1,72 @@
-from django.shortcuts import render
+from random import choice
+from string import ascii_lowercase, digits
+
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+from django.contrib.auth import get_user_model
 
 
-#TEMPERORY 
-def login(request):
-    return render (request, 'login.html')
+def already_used(username):
+
+    usermodel = get_user_model()
+    try:
+        usermodel.objects.get(username=username)
+        return True
+    except usermodel.DoesNotExist:
+        return False 
+
+
+def hash_username(length=16):
+
+    chars=ascii_lowercase+digits
+    username = ''
+    for token in range(length):
+        username += choice(chars)
+
+    if already_used(username):
+        hash_username()
+    else:
+        return username 
+
+
+def signup(request):
+
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        # valid input at this point
+        if form.is_valid():
+
+            email = form.cleaned_data.get('email')
+            raw_password = form.cleaned_data.get('password1')
+
+            user = form.save(commit=False)
+            user.username = hash_username()
+            user.set_password(raw_password)
+            user.email = email
+            user.save()
+
+            login(request, user)
+            
+            return redirect('/login')
+    else:
+        
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
 
 
 def index(request):
     return render(request, 'index.html')
 
-def blog(request):
-    return render(request, 'blog/blog.html')
 
-def data(request):
-    return render(request, 'blog/data.html')
+#TEMPERORY ( not valid forms with csrf_tokens)
+def login(request):
+    return render (request, 'login.html')
 
-def forum(request):
-    return render(request, 'blog/forum.html')
+
+def logout(request):
+    return render(request, 'logout.html')
+
+
+
