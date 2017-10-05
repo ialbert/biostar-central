4808de087756e3755f964e1947a8b3e1fffde22e
aswathyseb <aswathyseb@gmail.com>
2017-10-04 22:05:28 -0400

    run_job added

--- pipeline/run_job.py
+++ pipeline/run_job.py
@@ -1,8 +1,10 @@
-import subprocess,sys,os
+import subprocess,os
+import urllib.request
+#from engine.models import Job
 
 
-class Bunch:
-    pass
+DATA="http://iris.bx.psu.edu/projects/metabarcode-data/data.tar.gz"
+SINFO="http://iris.bx.psu.edu/projects/metabarcode-data/sampleinfo.txt"
 
 
 def run(job):
@@ -13,57 +15,50 @@ def run(job):
     spec = job.spec
     template = job.template
     outdir = job.outdir
-    id = job.id
+    jobid = job.jobid
 
     # create Makefile for analysis
-    completed = subprocess.run(['python','make.py', spec, template], stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    print(completed.check_returncode())
-
+    process1 = subprocess.run(['python','make.py', spec,template], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print(process1.check_returncode())
+    
     if not os.path.exists(outdir):
             os.system('mkdir -p  {0}'.format(outdir))
 
     mfile = open(os.path.join(outdir, "Makefile"), "w")
-    mfile.write(completed.stdout.decode('utf-8'))
+    mfile.write(process1.stdout.decode('utf-8'))
     mfile.close()
 
-    # get data
-
-
-    # run make
-    completed =subprocess.run(['make', 'check'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=outdir)
-    print(completed.stderr.decode('utf-8'))
-    print(completed.check_returncode())
-
-    print("{0] completed successfully!".format(id))
-
+    # get analysis data
+    urllib.request.urlretrieve(SINFO, os.path.join(outdir, 'sampleinfo.txt'))
+    urllib.request.urlretrieve(DATA, os.path.join(outdir, 'data.tar.gz'))
+    
+    # run analysis
+    process2 = subprocess.run(['make', 'all'], cwd=outdir)
+    print(process2.check_returncode())
+    
+    print("{0} ran successfully!".format(jobid))
     return
+
+
+class Job:
+    pass
 
 
 if __name__ == "__main__":
 
-    # job creation not needed later
+    # these won't be needed later.
+    ############# 
     spec = "templates/metabarcode_qc/metabarcode_spec.hjson"
     template = "templates/metabarcode_qc/metabarcode_makefile.html"
     out = "./workout"
+    jobid = "job0"
 
-    job= Bunch()
-    job.id = id
+    job = Job()
+    job.jobid= jobid
     job.spec = spec
     job.template = template
     job.outdir = out
-
-    completed =subprocess.run(['make', 'check'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=out)
-    print(completed.stderr.decode('utf-8'))
-    print(completed.check_returncode())
-    1/0
-
-    '''
-    c=subprocess.run(['touch','hello'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=out)
-    #data=c.stdout.decode('utf-8')
-    #cmd="cat {0} >testfile.txt'.format(data)"
-    #os.system(cmd)
-    1/0
-    '''
+    ##########
 
     run(job)
 
