bac45eda788a5f07b415161560ee423eba88f48d
Istvan Albert <istvan.albert@gmail.com>
2017-10-12 11:03:09 -0400

    Update by ialbert on Thu Oct 12 11:03:09 EDT 2017 from yolo.local

--- engine/templatetags/engine_tags.py
+++ engine/templatetags/engine_tags.py
@@ -1,6 +1,5 @@
-from django import template
 from django import forms
-from engine import models
+from django import template
 
 register = template.Library()
 
@@ -9,19 +8,26 @@ register = template.Library()
 def upper(value):
     return value.upper()
 
+
 @register.filter(name='date_sort')
 def date_sort(instance):
     return instance.order_by("-id")
+
 
 @register.inclusion_tag('widgets/breadcrumb.html')
 def breadcrumb(steps):
     return dict(steps=steps)
 
+
 @register.inclusion_tag('widgets/menubar.html')
-def menubar(project=None, edit_project=False,
+def menubar(project=None, edit_project=False, edit_data=False, data=None,
             create_project=False, upload_data=False):
-    return dict(project=project, edit_project=edit_project,
-                create_project=create_project, upload_data=upload_data)
+
+    return dict(
+        project=project, edit_project=edit_project, create_project=create_project,
+        data=data, edit_data=edit_data, upload_data=upload_data
+    )
+
 
 @register.filter(name='is_checkbox')
 def is_checkbox(value):
