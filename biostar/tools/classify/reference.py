147e5055790ce6999d1bb58899d723aff7a56622
aswathy <aswathyseb@gmail.com>
2017-10-24 17:04:36 -0400

    Update by asebastian on Tue Oct 24 17:04:36 EDT 2017 from Aswathys-iMac.local

--- biostar/tools/classify/reference.py
+++ biostar/tools/classify/reference.py
@@ -17,22 +17,23 @@ def accession_details(accessions):
     outputs: sequence.fa, acc2taxa.txt
     """
     # create a dictionary of list of the form
-    #d = { 'acc' : [species_name, sequence, taxon_id] }
+    # d = { 'acc' : [species_name, sequence, taxon_id] }
 
     accession_info = dict()
     Entrez.email = "name@abc.com"
     for accession in accessions:
         accession = accession.strip()
+
         try:
             with Entrez.efetch(db="nucleotide", rettype="fasta", retmode="fasta", id=accession) as handle:
                 seq_record = SeqIO.read(handle, "fasta")
-                species_name = ' '.join([seq_record.description.split()[1],seq_record.description.split()[2]])
-                accession_info[accession]=[species_name,seq_record.seq]
+                species_name = ' '.join([seq_record.description.split()[1], seq_record.description.split()[2]])
+                accession_info[accession] = [species_name,seq_record.seq]
 
         except Exception:
             print(f'{accession} gave error.{accession} discarded.')
 
-    if args.sequence:
+    if args.fasta:
         for k, v in accession_info.items():
             header = ">" + k
             seq = accession_info[k][1]
@@ -44,9 +45,7 @@ def accession_details(accessions):
             species = accession_info[k][0]
             taxon_id = os.popen("echo {0} | taxonkit name2taxid".format(species)).read().split('\t')[1]
             accession_info[k].append(taxon_id.strip())
-        for k, v in accession_info.items():
-            taxon_id = accession_info[k][2]
-            out = "\t".join([k,taxon_id])
+            out = "\t".join([k, taxon_id.strip()])
             print(out)
 
     return
@@ -57,7 +56,7 @@ if __name__ == "__main__":
 
     parser.add_argument("--ids", help="File with accessions.")
     parser.add_argument("--taxid", action='store_true',help="Print accession - taxid table.")
-    parser.add_argument("--sequence", action='store_true', help="Print fasta sequences of accessions.")
+    parser.add_argument("--fasta", action='store_true', help="Print fasta sequences of accessions.")
     args = parser.parse_args()
 
     if not args.ids:
