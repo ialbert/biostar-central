6e8b1e3f602ab2c0b2518689b9fb7bb4caedf388
Natay <b.natay@gmail.com>
2017-10-26 19:40:47 -0400

    Update by natay on Thu Oct 26 19:40:47 EDT 2017 from natay

--- biostar/emailer/models.py
+++ biostar/emailer/models.py
@@ -9,7 +9,7 @@ class EmailList(models.Model):
     ACTIVE, DELETED, NOT_ACTIVE = 1,2,3
 
     STATE_CHOICES = [(ACTIVE, "Active"), (DELETED, "Deleted"), (NOT_ACTIVE, "Not active")]
-    
+
     uid = models.CharField(max_length=16, blank=True, unique=True, default=get_uuid(16))
 
 
