0151d03a58bdd482d89c9dd704568cbf5f4705ec
aswathy <aswathyseb@gmail.com>
2017-10-26 15:46:36 -0400

    Update by asebastian on Thu Oct 26 15:46:36 EDT 2017 from Aswathys-iMac.local

--- biostar/tools/align/align.sh
+++ biostar/tools/align/align.sh
@@ -1,14 +1,35 @@
 set -ueo pipefail
 
 # Get parameters.
-INPUT_DATA={{data.path}}
-ACCESSION_LIST={{genome.path}}
-ORGANISM_NAME={{organism.name}}
+INPUT={{data.path}}
+GENOME={{genome.path}}
+ORGANISM={{organism.name}}
 
 # Internal parameters.
-REF_DIR=ref
-RESULT_DIR=results
+RESULTS=results
+DATA_DIR=$(dirname "$GENOME")
+INDEX=${DATA_DIR}/index
+OUTPUT="mapped.bam"
 
-# Build bwa index.
-bwa
+# Testing.
+
+echo -e "input data: $INPUT"
+echo -e "input genome: $GENOME"
+echo -e "DATA_DIR: $DATA_DIR"
+echo -e "INDEX: $INDEX"
+
+
+# Build bwa index if not exist in project data folder.
+if [ ! -f "$DATA_DIR/index.bwt" ]; then
+echo -e "Building bwa index.\n"
+bwa index -p index $GENOME
+fi
+
+# Align sequences
+echo -e "Mapping reads to genome.\n"
+bwa mem -t 4 $INDEX $INPUT | samtools view -Sb |samtools sort >${RESULTS}/$OUTPUT
+samtools index ${RESULTS}/$OUTPUT
+
+
+
 
