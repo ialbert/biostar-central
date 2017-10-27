d8ce82796291c62ed1a149046d6b2f0299a6d05b
aswathy <aswathyseb@gmail.com>
2017-10-26 21:24:39 -0400

    Update by aswathyseb on Thu Oct 26 21:24:39 EDT 2017 from MacBook-Air.local

--- biostar/tools/align/align.sh
+++ biostar/tools/align/align.sh
@@ -19,9 +19,19 @@ fi
 # Align sequences
 echo -e "Mapping reads to genome.\n"
 mkdir -p ${RESULTS}
-bwa mem -t 4 ${GENOME} ${INPUT} | samtools view -Sb |samtools sort >${RESULTS}/${OUTPUT}
-samtools index ${RESULTS}/${OUTPUT}
+bwa mem -t 4 ${GENOME} ${INPUT} | samtools view -b |samtools sort >${OUTPUT}
+samtools index ${OUTPUT}
 
+# Get total no. of reads in input
+TOTAL=$(bioawk -c fastx 'END{print NR}' ${INPUT})
+MAPPED_READS=$(samtools view -F 4 ${OUTPUT} | cut -f 1 | sort |uniq |wc -l)
 
+# Get number of reads mapped to each chromosome.
+IDX_STATS=$(samtools idxstats ${OUTPUT})
 
-
+# Collect results.
+mkdir -p $RESULTS
+mv ${OUTPUT}* ${RESULTS}/
+echo -e "Total\tMapped" >${RESULTS}/mapping_stats.txt
+echo -e "$TOTAL\t$MAPPED_READS" >>${RESULTS}/mapping_stats.txt
+echo -e "$IDX_STATS" >${RESULTS}/chrom_mapping.txt
