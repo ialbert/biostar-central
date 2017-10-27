8e5dba39d2ee9a090afe84bb47fad4ec9083392c
Natay <b.natay@gmail.com>
2017-10-27 12:50:41 -0400

    Update by natay on Fri Oct 27 12:50:41 EDT 2017 from natay

--- engine/views.py
+++ engine/views.py
@@ -86,7 +86,6 @@ def breadcrumb_builder(icons=[], project=None, analysis=None, data=None, job=Non
     return path
 
 
-# TODO: Replace with the manager thing.
 def project_list(request):
     projects = Project.objects.order_by("-id")
     if not request.user.is_superuser:
