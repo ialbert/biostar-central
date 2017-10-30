e6ddc6eb7b68ef76e694f44d247e6300590b5d46
Natay <b.natay@gmail.com>
2017-10-30 14:28:12 -0400

    Update by natay on Mon Oct 30 14:28:12 EDT 2017 from natay

--- biostar/engine/tests.py
+++ biostar/engine/tests.py
@@ -1,15 +1,29 @@
-
 from django.test import TestCase
 from django.test import Client
+from django.core.urlresolvers import get_resolver
+from .urls import urlpatterns
+
+
+
 
 class SiteTestCase(TestCase):
-    def setUp(self):
 
-        pass
 
     def test_site_pages(self):
         "Checking the site pages."
+        admin_project = 1
+        demo_project = 2
+
+        idpattern = "(?P<id>\d+)"
+        pathpattern = "(?P<path>.+)"
+
+
         c = Client()
+        #patterns = get_resolver(urlpatterns)
+        for pat in urlpatterns:
+            print(pat.regex.pattern)
+        1/0
+
         resp = c.post('/')
         self.assertEqual(resp.status_code, 200)
 
