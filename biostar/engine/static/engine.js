27ffcd318c758c4c0cc46b8f66c28ba3e7a5440f
Istvan Albert <istvan.albert@gmail.com>
2017-11-02 14:51:39 -0400

    Update by ialbert on Thu Nov  2 14:51:39 EDT 2017 from yolo.local

--- biostar/engine/static/engine.js
+++ biostar/engine/static/engine.js
@@ -3,4 +3,10 @@ $(document).ready(function () {
     $('select.dropdown')
         .dropdown()
     ;
+
+    $(".item").click(function (event) {
+        var obj = $(this).find("a:first");
+        window.location = obj.attr("href");
+    });
+
 });
