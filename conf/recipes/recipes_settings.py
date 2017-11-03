b6be11786b47dae89ed0d787267aee8aa8a5f759
Istvan Albert <istvan.albert@gmail.com>
2017-11-03 19:46:15 -0400

    Update by ialbert on Fri Nov  3 19:46:15 DST 2017 from beep

--- conf/recipes/recipes_settings.py
+++ conf/recipes/recipes_settings.py
@@ -3,6 +3,8 @@ from .recipes_secrets import *
 
 DEBUG = False
 
-WSGI_APPLICATION = 'conf.main.main_wsgi.application'
+WSGI_APPLICATION = 'conf.recipes.recipes_wsgi.application'
 
 ALLOWED_HOSTS = ['bioinformatics.recipes', 'www.bioinformatics.recipes']
+
+SITE_HEADER =  "Bioinformatics Recipes"
