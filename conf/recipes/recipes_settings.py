7d9c9cd7dd19e82006b6d082df99adb85eec7d9b
Istvan Albert <istvan.albert@gmail.com>
2017-11-03 20:15:14 -0400

    Update by ialbert on Fri Nov  3 20:15:14 DST 2017 from beep

--- conf/recipes/recipes_settings.py
+++ conf/recipes/recipes_settings.py
@@ -1,6 +1,8 @@
 from biostar.engine.settings import *
-from .recipes_secrets import *
-
+try:
+    from .recipes_secrets import *
+except ImportError as exc:
+    print("No recipes.recipes_secrets module could be found")
 DEBUG = False
 
 WSGI_APPLICATION = 'conf.recipes.recipes_wsgi.application'
