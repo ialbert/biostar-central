e9249bb26a81801afa64e5dea4dbf9e137138a75
Istvan Albert <istvan.albert@gmail.com>
2017-11-11 11:06:16 -0500

    Update by ialbert on Sat Nov 11 11:06:15 STD 2017 from beep

--- biostar/tools/fastqc/fastqc.sh
+++ biostar/tools/fastqc/fastqc.sh
@@ -1,6 +1,7 @@
+# Stop script on any error.
 set -ueo pipefail
 
-# Locates all files in this directory
+# The directory that contains the data.
 DATA_DIR=$(dirname {{sequence.path}})
 
 # The output directory
@@ -8,10 +9,17 @@ OUTPUT=fastqc_results
 
 # Create the output directory
 mkdir -p $OUTPUT
-mkdir -p $OUTPUT/zip
 
-# Select all files in the directory that match fastq/fq
-cat {{sequence.toc}} | egrep ".fastq|.fastq.gz|.fq|.fq.gz" | parallel -j 5 fastqc -o $OUTPUT {}
+# Select all files in the directory that match a known pattern.
+cat {{sequence.toc}} | egrep ".fastq|.fastq.gz|.fq|.fq.gz|.bam" | parallel -j 5 fastqc -o $OUTPUT {}
 
 # Move the zip files out of sight
+mkdir -p $OUTPUT/zip
 mv $OUTPUT/*.zip $OUTPUT/zip
+
+# These variables need to be set for multiqc
+LC_ALL=C.UTF-8
+LANG=C.UTF-8
+
+# Generate a multiqc report.
+multiqc fastqc_results -o fastqc_results
