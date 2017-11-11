1ab51f7b29b19190ade75fe84b2b3007650aa258
Istvan Albert <istvan.albert@gmail.com>
2017-11-11 09:54:45 -0500

    Update by ialbert on Sat Nov 11 09:54:45 STD 2017 from beep

--- biostar/engine/tasks.py
+++ biostar/engine/tasks.py
@@ -39,8 +39,6 @@ try:
         logger.info(f"SPOOL SUBMIT {COUNTER}, {COUNTER+1}, {COUNTER+2}")
         demo.spool(value=COUNTER)
         time.sleep(100)
-        return
-
         demo.spool(value=COUNTER+1)
         time.sleep(1)
         demo.spool(value=COUNTER+2)
