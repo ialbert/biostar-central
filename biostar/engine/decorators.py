6ec56f7ab40a938d39bd301c40ad3312e5583bc8
Natay <b.natay@gmail.com>
2017-11-13 22:41:31 -0500

    Update by natay on Mon Nov 13 22:41:31 EST 2017 from natay

--- biostar/engine/decorators.py
+++ biostar/engine/decorators.py
@@ -1,4 +1,3 @@
-from .models import Project
 from django.contrib import messages
 from django.urls import reverse
 from django.shortcuts import redirect
@@ -19,23 +18,26 @@ class object_access:
 
         def wrap(request, *args, **kwargs):
             id, user = kwargs['id'], request.user
+            try:
+                self.redirect_url = self.redirect_url or self.instance.url()
+            except:
+                self.redirect_url = self.redirect_url or reverse("project_list")
 
             query = self.instance.objects.filter(pk=id).first()
-
             project, instance, allow_access = auth.check_obj_access(user, query,
                                                            owner_only=self.owner_only)
 
             # Everything has a project associated with it
             if not project:
                 messages.error(request, f"Project with id={id} does not exist.")
-                return redirect(reverse("project_list"))
+                return redirect(self.redirect_url)
 
             if allow_access:
                 return function(request, *args, **kwargs)
             else:
                 #TODO: the redirection is generic and annoying
-                messages.error(request, f"Access to {self.instance.__name__}={id} denied.")
-                return redirect(reverse("project_list"))
+                messages.error(request, f"Access/modification to {self.instance.__name__}={id} denied.")
+                return redirect(self.redirect_url)
 
         wrap.__doc__ = function.__doc__
         wrap.__name__ = function.__name__
