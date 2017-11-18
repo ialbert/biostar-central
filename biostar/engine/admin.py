e1f868cb8e93cf22859fff82f3765eddb5e18f34
Natay <b.natay@gmail.com>
2017-11-18 13:55:06 -0500

    Update by natay on Sat Nov 18 13:55:06 EST 2017 from natay

--- biostar/engine/admin.py
+++ biostar/engine/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 from .models import Job, Analysis
 
 
-admin.site.register(Job)
-admin.site.register(Analysis)
+
+
+class JobAdmin(admin.ModelAdmin):
+
+    search_fields = ('name', 'owner__first_name', 'owner__email', 'state')
+    list_display = ("name", "state","start_date", "security","date")
+    list_filter = ("state", "security")
+
+
+class AnalysisAdmin(admin.ModelAdmin):
+
+    search_fields = ('name', 'text', 'owner__first_name', 'owner__email' )
+    list_display = ("name", "date", "auth")
+    list_filter = ( "state", "auth")
+
+
+admin.site.register(Job, JobAdmin)
+admin.site.register(Analysis, AnalysisAdmin)
