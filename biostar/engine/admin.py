35a7cf1e5ebc698bcb0e69e70103eceb18f6ec39
Natay <b.natay@gmail.com>
2017-11-18 15:36:25 -0500

    Update by natay on Sat Nov 18 15:36:24 EST 2017 from natay

--- biostar/engine/admin.py
+++ biostar/engine/admin.py
@@ -3,20 +3,29 @@ from .models import Job, Analysis
 
 
 
-
+@admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
 
     search_fields = ('name', 'owner__first_name', 'owner__email', 'state')
     list_display = ("name", "state","start_date", "security","date")
-    list_filter = ("state", "security")
+    list_filter = ("state", "security", "project__name")
 
 
+    fieldsets = ((None,
+                    {'fields': ("name","owner",'project',("uid","sticky"),
+                                ("state", "security"))}),
+
+                 ("Run Time Settings",
+                    {'fields':("json_text", "template"),
+                     "classes": ("wide", 'extrapretty')},
+                  ),
+                 )
+
+@admin.register(Analysis)
 class AnalysisAdmin(admin.ModelAdmin):
 
     search_fields = ('name', 'text', 'owner__first_name', 'owner__email' )
     list_display = ("name", "date", "auth")
-    list_filter = ( "state", "auth")
+    list_filter = ( "state", "auth", "project__name")
 
 
-admin.site.register(Job, JobAdmin)
-admin.site.register(Analysis, AnalysisAdmin)
