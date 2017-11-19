2362dce9185393fbe7adca1fb0e846da16170fc3
Natay <b.natay@gmail.com>
2017-11-19 13:38:49 -0500

    Update by natay on Sun Nov 19 13:38:49 EST 2017 from natay

--- biostar/engine/admin.py
+++ biostar/engine/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Job, Analysis
 from django.forms import TextInput, Textarea
-from django.db.models import CharField, TextField
+from django.db.models import TextField
 
 
 
@@ -9,18 +9,17 @@ from django.db.models import CharField, TextField
 class JobAdmin(admin.ModelAdmin):
 
     formfield_overrides = {
-        #CharField: {'widget': TextInput(attrs={'size': '20'})},
         TextField: {'widget': Textarea(attrs={'rows':20,'cols': 100})},
     }
 
-    search_fields = ('name', 'owner__first_name', 'owner__email', 'state')
+    search_fields = ('name', 'owner__first_name', 'owner__email', 'state', "project__name")
     list_display = ("name", "state","start_date", "security","date")
     list_filter = ("state", "security", "project__name")
 
 
-    fieldsets = (("Analysis Metadata",
+    fieldsets = (("Job Metadata",
                     {'fields': ("name","owner",'project',("uid","sticky"),
-                                ("state", "security")),
+                                ("state", "security"), "image"),
                      "classes": ('extrapretty')}
                   ),
 
@@ -39,8 +38,28 @@ class JobAdmin(admin.ModelAdmin):
 @admin.register(Analysis)
 class AnalysisAdmin(admin.ModelAdmin):
 
+    formfield_overrides = {
+        TextField: {'widget': Textarea(attrs={'rows':20,'cols': 100})},
+    }
+
     search_fields = ('name', 'text', 'owner__first_name', 'owner__email' )
     list_display = ("name", "date", "auth")
     list_filter = ( "state", "auth", "project__name")
 
 
+    fieldsets = (("Analysis Metadata",
+                    {'fields': ("name","owner",'project',("uid","sticky"),
+                                ("state", "auth")),
+                     "classes": ('extrapretty')}
+                  ),
+
+                 ("Optional Text Inputs",
+                    {'fields':(("text", "html"),"summary"),
+                     "classes": ("collapse",'extrapretty')}
+                  ),
+
+                 ("Run Time Settings",
+                    {'fields':("json_text", "template"),
+                     "classes": ("wide", 'extrapretty')},
+                  ),
+                 )
--- biostar/engine/forms.py
+++ biostar/engine/forms.py
@@ -287,6 +287,7 @@ class EditAnalysisForm(forms.Form):
 
         self.analysis.json_text = self.cleaned_data["json_text"]
 
+        #TODO: test more
         if self.analysis.template != self.cleaned_data["template"]:
             self.analysis.auth = Analysis.UNDER_REVIEW
 
