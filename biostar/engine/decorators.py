687466ffcc84e0105ae5d96112214c716e40f0b4
Istvan Albert <istvan.albert@gmail.com>
2017-11-22 11:22:03 -0500

    Update by ialbert on Wed Nov 22 11:22:03 EST 2017 from yolo.local

--- biostar/engine/decorators.py
+++ biostar/engine/decorators.py
@@ -7,29 +7,38 @@ from . import auth
 from django.utils.decorators import available_attrs
 
 
+LOGIN_REQUIRED_MESSAGE = '''
+You have to be logged in
+'''
+
 class object_access:
 
     # Initialize with an 'instance'= Project, Job, Data, or Analysis
     # redirects to self.instance.url() if no redirect_url is given
     # and if self.instance doesnt have a url() method the default is project_list.
-    def __init__(self, instance, owner_only=False, redirect_url=None):
+    def __init__(self, instance, owner_only=False, redirect_url="/"):
         self.instance = instance
         self.owner_only = owner_only
         self.redirect_url = redirect_url
 
     def __call__(self, function, *args, **kwargs):
         """
-           Decorator used to test if a user has rights to access an instance
+        Decorator used to test if a user has rights to access an instance
         """
 
         # Pass function attributes to the wrapper...using a wrapper
         @wraps(function, assigned=available_attrs(function))
         def _wrapped_view(request, *args, **kwargs):
-            id, user = kwargs['id'], request.user
+
+            id = kwargs['id']
+            user = request.user
+
             query = self.instance.objects.filter(pk=id).first()
+
             # Every query has to have a valid project ( query.project exists)
-            project, query, allow_access = auth.check_obj_access(user, query,
-                                                           owner_only=self.owner_only)
+            project, query, allow_access = auth.check_obj_access(user, query, owner_only=self.owner_only)
+
+
             try:
                 # Catch failure if instance doesnt have url() method
                 self.redirect_url = self.redirect_url or query.url()
