6b121ea8dc03db1e3da29fde32d6574d95ba9220
Natay <b.natay@gmail.com>
2018-01-16 15:49:03 -0500

    Update by natay on Tue Jan 16 15:48:57 EST 2018 from natay

--- biostar/engine/auth.py
+++ biostar/engine/auth.py
@@ -356,6 +356,9 @@ def create_data(project, user=None, stream=None, path='', name='',
         collect = findfiles(path, collect=[])
         for src in collect:
             dest = create_path(fname=src, data=data)
+
+            #TODO: bug here ( shows when adding hello-world analyis)
+
             os.symlink(src, dest)
         summary = f'Contains {len(collect)} files. {summary}'
         logger.info(f"Linked {len(collect)} files.")
--- biostar/tools/hello/hello4.sh
+++ biostar/tools/hello/hello4.sh
@@ -1,8 +1,10 @@
 # Help the user track progress.
 echo "Analysis started."
-
 # Run a simple unix tool on the file.
-ls -l {{data.path}} > results.txt
+echo "{{data.value}}" > results.txt
+
+# Make a nested directory structure
+mkdir -p multiple/dirs/produced
 
 # All done.
 echo "Analysis finished."
