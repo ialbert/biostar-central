9b6dc5b24bf0aaecd09b6d8ad98901490cde0b08
Istvan Albert <istvan.albert@gmail.com>
2018-01-18 20:29:20 -0500

    Update by ialbert on Thu Jan 18 20:29:20 EST 2018 from beep

--- biostar/tools/hello/hello3.sh
+++ biostar/tools/hello/hello3.sh
@@ -3,3 +3,10 @@ echo "The selected cutoff was : {{cutoff.value}}"
 
 echo "The selected organism was: {{organism.value}}"
 
+# Print environment related information.
+
+echo "--- Environment ---"
+echo "PATH=$PATH"
+echo "USER=$USER"
+echo "HOME=$HOME"
+echo "PWD=`pwd`"
