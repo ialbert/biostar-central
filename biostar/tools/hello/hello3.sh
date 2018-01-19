847727b978eedd5a2907d329d0b98c10cd47eb30
Istvan Albert <istvan.albert@gmail.com>
2018-01-18 20:41:11 -0500

    Update by ialbert on Thu Jan 18 20:41:10 EST 2018 from beep

--- biostar/tools/hello/hello3.sh
+++ biostar/tools/hello/hello3.sh
@@ -5,8 +5,6 @@ echo "The selected organism was: {{organism.value}}"
 
 # Print environment related information.
 
-echo "--- Environment ---"
-echo "PATH=$PATH"
-echo "USER=$USER"
-echo "HOME=$HOME"
-echo "PWD=`pwd`"
+echo "--- Print Environment ---"
+printenv
+
