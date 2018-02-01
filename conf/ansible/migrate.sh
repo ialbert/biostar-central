1ba6183c39f6f124d522a9e48427f207a2d491a0
Istvan Albert <istvan.albert@gmail.com>
2018-02-01 14:27:41 -0500

    Update by ialbert on Thu Feb  1 14:27:41 EST 2018 from yolo.local

--- conf/ansible/migrate.sh
+++ conf/ansible/migrate.sh
@@ -9,3 +9,4 @@ DJANGO_SETTINGS_MODULE=conf.site.site_settings
 python manage.py migrate
 python manage.py collectstatic --noinput -v 0
 python manage.py project --json initial/tutorial/tutorial-project.hjson --privacy public --jobs
+python manage.py project --root ../biostar-recipes --json projects/cookbook/cookbook-project.hjson --privacy public --jobs
