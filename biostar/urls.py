e3c487ed94f593f1ca709e74d4d950bed9188f14
Natay <b.natay@gmail.com>
2018-02-16 00:14:26 -0500

    Update by natay on Fri Feb 16 00:14:26 EST 2018 from natay

--- biostar/urls.py
+++ biostar/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
 
 ]
 
-#if settings.DEBUG:
-#    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, show_indexes=False)
-#    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT, show_indexes=False)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT, show_indexes=False)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT, show_indexes=False)
 
