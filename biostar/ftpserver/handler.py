e3be039c25253b0d4e94e33f01af08b991f865ec
Natay <b.natay@gmail.com>
2018-04-11 16:17:37 -0400

    Update by natay on Wed Apr 11 16:17:35 EDT 2018 from natay

--- biostar/ftpserver/handler.py
+++ biostar/ftpserver/handler.py
@@ -6,7 +6,7 @@ from itertools import chain
 from biostar.engine import auth, models
 
 
-from .util import parse_virtual_path
+from .util import parse_virtual_path, query_tab
 
 
 logger = logging.getLogger("engine")
@@ -71,28 +71,40 @@ class BiostarFTPHandler(FTPHandler):
 
         # Creating a directory at the root dir
         if root_project and not tab:
+
             if projects.filter(name=root_project):
                 self.respond('550 Directory already exists.')
                 return
             else:
                 # Create a new project
                 project = auth.create_project(user=user, name=root_project)
-                self.fs.projects = chain(self.fs.projects, models.Project.objects.filter(pk=project.pk))
+                self.fs.projects = chain(projects, models.Project.objects.filter(pk=project.pk))
 
                 line = self.fs.fs2ftp(path)
                 self.respond('257 "%s" directory created.' % line.replace('"', '""'))
 
             return path
 
-        project = projects.filter(name=root_project)
+        if tab == "data" and name and not tail:
 
-        if name and not tail:
-            logger.info(f"path={path}")
-            1/0
+            instance = query_tab(tab=tab, project=root_project, name=name)
+            if instance:
+                self.respond('550 Directory already exists.')
+                return
+            else:
+                project = self.fs.projects.filter(name=root_project)
+                data = auth.create_data(project=project.first(), user=user, name=name)
+                self.fs.data = chain(self.fs.data, models.Data.objects.filter(pk=data.pk))
 
+                line = self.fs.fs2ftp(path)
+                self.respond('257 "%s" directory created.' % line.replace('"', '""'))
+
+                logger.info(f"path={path}")
+
+            return path
 
         logger.info(f"new_dir={path}, path={path}, root_project={root_project}, project={projects.filter(name=root_project)}")
 
-        #1/0
+        1/0
         return path
 
