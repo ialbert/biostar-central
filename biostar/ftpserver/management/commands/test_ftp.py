3b0fb5cdf50908fa9c2f85674d05d51e3d3fc192
Natay <b.natay@gmail.com>
2018-04-12 18:04:37 -0400

    Update by natay on Thu Apr 12 18:04:36 EDT 2018 from natay

--- biostar/ftpserver/management/commands/test_ftp.py
+++ biostar/ftpserver/management/commands/test_ftp.py
@@ -1,10 +1,33 @@
+from django.core.management.base import BaseCommand
+
 from ftplib import FTP
+#from biostar.engine import auth
 from biostar.settings import *
 
-ftp = FTP()     # connect to host, default port
 
 
-ftp.connect(host=FTP_HOST, port=FTP_PORT)
 
 
-ftp.login()
+class Command(BaseCommand):
+    help = "Add users"
+
+
+    def handle(self, *args, **options):
+        #from biostar.accounts.models import User
+        ftp = FTP()     # connect to host, default port
+
+
+        ftp.connect(host=FTP_HOST, port=FTP_PORT)
+
+        ftp.login(user=ADMINS[0][1], passwd=DEFAULT_ADMIN_PASSWORD)
+
+        #user= User.objects.filter(email=ADMINS[0][1]).first()
+        #projects = auth.get_project_list(user=user)
+
+        files = list(ftp.mlsd(path="/"))
+
+        for f in files:
+            print(f[0])
+
+        #print(files, projects)
+
