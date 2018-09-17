1018a5c6b4c1c6160601d84f292dfffbeab51bcf
Istvan Albert <istvan.albert@gmail.com>
2018-09-17 11:53:57 -0400

    Update by ialbert on Mon Sep 17 11:53:57 EDT 2018 from yolo.local

--- biostar/engine/admin.py
+++ biostar/engine/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from .models import Job, Analysis, Project
+from .models import Job, Analysis, Project, Access
 from django.forms import Textarea
 from django.db.models import TextField
 
 
+@admin.register(Access)
+class AccessAdmin(admin.ModelAdmin):
+    list_select_related = (
+        'user',
+        'project',
+    )
+
+    pass
 
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
