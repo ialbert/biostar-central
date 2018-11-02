50734d895183ab70bd3278ec25bff6e05e55b297
Natay Aberra <natay.aberra@gmail.com>
2018-11-02 07:34:08 -0400

    Update by natay on Fri Nov  2 07:34:08 EDT 2018 from Admins-MacBook-Air.local

--- biostar/engine/api.py
+++ biostar/engine/api.py
@@ -1,12 +1,11 @@
 
 from .models import Analysis
 from django.http import HttpResponse
-from .const import *
 
 
 def api_text(recipe, text_type):
 
-    action_mapper = {JSON: recipe.json_text, TEMPLATE: recipe.template}
+    action_mapper = {"json": recipe.json_text, "template": recipe.template}
     # Only show public recipes for now.
     text = action_mapper.get(text_type, "") if recipe and recipe.project.is_public else ""
     return HttpResponse(text, content_type="text/plain")
@@ -16,13 +15,13 @@ def api_recipe_json(request, uid):
     """Returns plain text version of json"""
 
     recipe = Analysis.objects.filter(uid=uid).first()
-    return api_text(recipe=recipe, text_type=JSON)
+    return api_text(recipe=recipe, text_type="json")
 
 
 def api_recipe_template(request, uid):
     """Returns plain text version of template"""
 
     recipe = Analysis.objects.filter(uid=uid).first()
-    return api_text(recipe=recipe, text_type=TEMPLATE)
+    return api_text(recipe=recipe, text_type="template")
 
 
