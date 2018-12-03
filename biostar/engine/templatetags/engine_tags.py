ef484c7130b1f92fbf4a9a0c6067969777c8e445
Natay Aberra <natay.aberra@gmail.com>
2018-12-03 17:44:44 -0500

    Update by natay on Mon Dec  3 17:44:44 EST 2018 from Admins-MacBook-Air.local

--- biostar/engine/templatetags/engine_tags.py
+++ biostar/engine/templatetags/engine_tags.py
@@ -282,12 +282,11 @@ def recipe_form(form):
     return dict(form=form)
 
 @register.inclusion_tag('widgets/created_by.html')
-def created_by(date, user, obj=None):
+def created_by(date, user, obj=None, detail=False):
     """
     Renders a created by link
     """
-
-    return dict(date=date, user=user, obj=obj)
+    return dict(date=date, user=user, obj=obj, detail=detail)
 
 @register.inclusion_tag('widgets/access_form.html')
 def access_form(project, user, form):
