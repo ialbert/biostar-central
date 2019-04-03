3eead184017eb4c52dfb672fc1d517738b000e0c
Natay Aberra <natay.aberra@gmail.com>
2019-04-03 19:44:40 -0400

    Update by natay on Wed Apr  3 19:44:40 EDT 2019 from Admins-MacBook-Air.local

--- biostar/transfer/management/commands/transfer.py
+++ biostar/transfer/management/commands/transfer.py
@@ -11,34 +11,45 @@ logger = logging.getLogger("engine")
 
 def copy_users():
 
-    source = UsersUser.objects.all()
+    def create_users():
+        source = UsersUser.objects.all()
 
-    for user in source:
+        # Bulk create, then bulk update.
+        for user in source:
+            new_user = User.objects.filter(email=user.email).first()
+            if new_user:
+                continue
+            username = f"{user.name}{user.id}"
+            # Create user
+            yield User(username=username, email=user.email, password=user.password,
+                       is_active=user.is_active, is_superuser=user.is_admin, is_staff=user.is_staff)
 
-        new_user = User.objects.filter(email=user.email).first()
-        if new_user:
-            continue
+    def create_profile():
 
-        username = f"{user.name}{user.id}" if User.objects.filter(username=user.name).exists() else user.name
+        source = UsersUser.objects.all()
+        for user in source:
+            # Update profile
+            profile = Profile.objects.filter(uid=user.id).first()
+            if profile:
+                continue
+            new_user = User.objects.filter(email=user.email).first()
+            yield Profile(uid=user.id, name=user.name, user=new_user,
+                          role=user.type, last_login=user.last_login,
+                          date_joined=user.profile.date_joined, location=user.profile.location,
+                          website=user.profile.website, scholar=user.profile.scholar, text=user.profile.info,
+                          score=user.score, twitter=user.profile.twitter_id, my_tags=user.profile.my_tags,
+                          digest_prefs=user.profile.digest_prefs, new_messages=user.new_messages)
 
-        # Create user
-        new_user = User.objects.create(username=username, email=user.email,
-                                       password=user.password, is_active=user.is_active,
-                                       is_superuser=user.is_admin, is_staff=user.is_staff)
-        # Update profile
-        Profile.objects.filter(user=new_user).update(uid=user.id, name=user.name,
-                              role=user.type, last_login=user.last_login,
-                              date_joined=user.profile.date_joined, location=user.profile.location,
-                              website=user.profile.website, scholar=user.profile.scholar, text=user.profile.info,
-                              score=user.score, twitter=user.profile.twitter_id, my_tags=user.profile.my_tags,
-                              digest_prefs=user.profile.digest_prefs, new_messages=user.new_messages)
+    # Bulk create the users, then profile.
+    User.objects.bulk_create(objs=create_users(), batch_size=10000)
+    Profile.objects.bulk_create(objs=create_profile(), batch_size=10000)
 
     logger.info("Copied all users from biostar2")
 
 
 def copy_posts(relations={}):
     logger.info("Copying posts.")
-    source = PostsPost.objects.all().order_by("pk")[:10000]
+    source = PostsPost.objects.all().order_by("pk")[:100000]
 
     for post in source:
         new_post = Post.objects.get_all(uid=post.id).first()
@@ -64,35 +75,13 @@ def copy_posts(relations={}):
     logger.info("Copied all posts from biostar2")
 
 
-def update_posts(relations):
-
-    #Walk through forum tree and set the root, parent relations.
-
-    def generate():
-
-        for post_uid in relations:
-            root_uid, parent_uid = relations[post_uid][0], relations[post_uid][1]
-            post = Post.objects.filter(uid=post_uid).first()
-            root = Post.objects.filter(uid=root_uid).first()
-            parent = Post.objects.filter(uid=parent_uid).first()
-
-            if not (root and parent):
-                continue
-            post.root = root
-            post.parent = parent
-            yield post
-
-    Post.objects.bulk_update(objs=generate(), fields=["root", "parent"], batch_size=100)
-    return
-
-
 def copy_votes():
 
     source = PostsVote.objects.all()[:1000]
     for vote in source:
         author = User.objects.filter(profile__uid=vote.author_id).first()
         post = Post.objects.get_all(uid=vote.post_id).first()
-        new_vote = Vote.objects.filter(author=author, post=post, type=vote.type).first()
+        new_vote = Vote.objects.filter(uid=vote.id).first()
         # Skip if vote already exists.
         if new_vote:
             continue
@@ -119,26 +108,40 @@ def copy_and_update():
     # Walk through tree and update parent, root, post, relationships
     logger.info("Updating posts")
 
-    for post_uid in relations:
-        root_uid, parent_uid = relations[post_uid][0], relations[post_uid][1]
-        root = Post.objects.filter(uid=root_uid).first()
-        parent = Post.objects.filter(uid=parent_uid).first()
-        Post.objects.filter(uid=post_uid).update(parent=parent, root=root)
+    def generate_counts():
 
-        # Parent and root do not exist
-        if not (root and parent):
-            continue
+        for post_uid in relations:
+            post = Post.objects.filter(uid=post_uid).first()
+            post.root.thread_users.remove(post.author)
+            post.root.thread_users.add(post.author)
+            thread_query = Post.objects.filter(status=Post.OPEN, root=post.root)
 
-        post = Post.objects.filter(uid=post_uid).first()
-        thread = root if root is not None else post
-        thread.thread_users.remove(post.author)
-        thread.thread_users.add(post.author)
-        thread_query = Post.objects.filter(status=Post.OPEN, root=root)
+            if post.parent == post:
+                reply_count = thread_query.exclude(uid=post.parent.uid).filter(type=Post.ANSWER).count()
+                post.reply_count = reply_count
 
-        reply_count = thread_query.exclude(uid=parent.uid).filter(type=Post.ANSWER).count()
-        thread_score = thread_query.exclude(uid=root.uid).count()
-        Post.objects.filter(parent=parent).update(reply_count=reply_count)
-        Post.objects.filter(root=root).update(thread_score=thread_score)
+            if post.is_toplevel:
+                thread_score = thread_query.exclude(uid=post.uid).count()
+                post.thread_score = thread_score
+
+            yield post
+
+    def generate():
+
+        for post_uid in relations:
+            root_uid, parent_uid = relations[post_uid][0], relations[post_uid][1]
+            post = Post.objects.filter(uid=post_uid).first()
+            root = Post.objects.filter(uid=root_uid).first()
+            parent = Post.objects.filter(uid=parent_uid).first()
+
+            if not root:
+                continue
+            post.root = root
+            post.parent = parent
+            yield post
+
+    Post.objects.bulk_update(objs=generate(), fields=["root", "parent"], batch_size=100)
+    Post.objects.bulk_update(objs=generate_counts(), fields=["thread_score", "reply_count"], batch_size=100)
 
 
 class Command(BaseCommand):
@@ -150,8 +153,10 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        #copy_users()
-        copy_and_update()
+        copy_users()
+        #
+
+        #copy_and_update()
         #Vote.objects.bulk_create(objs=copy_votes(), batch_size=20)
 
         return
