74ce24910e208a1c23058d4013b186123676dd56
Natay Aberra <natay.aberra@gmail.com>
2019-04-08 11:22:22 -0400

    Update by natay on Mon Apr  8 11:22:22 EDT 2019 from Admins-MacBook-Air.local

--- biostar/forum/models.py
+++ biostar/forum/models.py
@@ -41,20 +41,14 @@ class PostManager(models.Manager):
 
         # Prefetch tags and thread user info
         query = query.prefetch_related("thread_users__profile", "thread_users")
-        query = query.select_related("root",  "lastedit_user", "lastedit_user__profile")
+        query = query.select_related("root",  "author", "author__profile",
+                                     "lastedit_user", "lastedit_user__profile")
         return query
 
-    def get_queryset(self):
-        "Regular queries exclude deleted stuff"
-        query = self.select_prefetch(super().get_queryset())
-        return query
-
-    def get_discussions(self, **kwargs):
-        "Get posts exclusively tied to projects"
-
-        query = super().get_queryset().exclude(project=None, status=Post.DELETED).filter(**kwargs)
-        query = self.select_prefetch(query.select_related("project"))
-        return query
+    #def get_queryset(self):
+    #    "Regular queries exclude deleted stuff"
+    #    query = self.select_prefetch(super().get_queryset())
+    #    return query
 
     def get_all(self, **kwargs):
         "Return everything"
@@ -92,35 +86,6 @@ class PostManager(models.Manager):
         query = self.select_prefetch(query).order_by("-creation_date")
         return query
 
-    def tag_search(self, text, defer_content=True):
-        "Performs a query by one or more , separated tags"
-
-        include, exclude = [], []
-        # Split the given tags on ',' and '+'.
-        terms = text.split(',') if ',' in text else text.split('+')
-        fixcase = lambda text:text.upper() if len(text) == 1 else text.lower()
-        for term in terms:
-            term = term.strip()
-            if term.endswith("!"):
-                exclude.append(fixcase(term[:-1]))
-            else:
-                include.append(fixcase(term))
-
-        if include:
-            query = self.filter(type__in=Post.TOP_LEVEL, tag_val__iregex=include).exclude(
-                tags__name__in=exclude)
-        else:
-            query = self.filter(type__in=Post.TOP_LEVEL).exclude(tag_val__iregex=exclude)
-
-        query = query.filter(status=Post.OPEN)
-        if defer_content:
-            # Remove fields that are not used.
-            query = query.defer('content', 'html')
-
-        # Get the tags.
-        query = self.select_prefetch(query=query).distinct()
-        return query
-
     def get_thread(self, root, user):
         # Populate the object to build a tree that contains all posts in the thread.
         is_moderator = user.is_authenticated and user.profile.is_moderator
@@ -128,7 +93,7 @@ class PostManager(models.Manager):
         query = super().get_queryset().filter(root=root)
         query = query.select_related("root", "root__author", "root__author__profile", "author", "author__profile",
                                      "lastedit_user", "lastedit_user__profile")
-        query = query if is_moderator else query.filter(~Q(status=Post.DELETED))
+        query = query if is_moderator else query.exclude(status=Post.DELETED)
 
         query = query.order_by("type", "-has_accepted", "-vote_count", "creation_date")
         return query
@@ -137,11 +102,6 @@ class PostManager(models.Manager):
         "Returns posts based on a user type"
         is_moderator = user.is_authenticated and (user.profile.is_moderator or user.profile.is_manager)
 
-        #query = query.prefetch_related("thread_users__profile", "thread_users")
-        #query = query.select_related("root", "lastedit_user", "lastedit_user__profile")
-
-        #print(query.explain())
-        #1/0
         query = super().get_queryset().filter(type__in=Post.TOP_LEVEL)
         query = query.prefetch_related("root",  "lastedit_user", "lastedit_user__profile",
                                        "thread_users__profile", "thread_users")
