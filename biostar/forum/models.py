3ed73e842281694a47f090708fc35857fc68e0df
Natay Aberra <natay.aberra@gmail.com>
2019-04-08 12:06:44 -0400

    Update by natay on Mon Apr  8 12:06:44 EDT 2019 from Admins-MacBook-Air.local

--- biostar/forum/models.py
+++ biostar/forum/models.py
@@ -103,6 +103,7 @@ class PostManager(models.Manager):
         is_moderator = user.is_authenticated and (user.profile.is_moderator or user.profile.is_manager)
 
         query = super().get_queryset().filter(type__in=Post.TOP_LEVEL)
+
         query = query.prefetch_related("root",  "lastedit_user", "lastedit_user__profile",
                                        "thread_users__profile", "thread_users")
         query = query if is_moderator else query.exclude(status=Post.DELETED)
@@ -237,6 +238,10 @@ class Post(models.Model):
     @property
     def is_open(self):
         return self.status == Post.OPEN
+
+    @property
+    def is_deleted(self):
+        return self.status == Post.DELETED
 
     @property
     def is_comment(self):
