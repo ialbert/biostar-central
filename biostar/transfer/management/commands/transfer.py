987df430b5119c0ac94c336a5214c2d5146ab641
Natay Aberra <natay.aberra@gmail.com>
2019-04-17 18:11:55 -0400

    Update by natay on Wed Apr 17 18:11:55 EDT 2019 from Admins-MacBook-Air.local

--- biostar/transfer/management/commands/transfer.py
+++ biostar/transfer/management/commands/transfer.py
@@ -122,29 +122,35 @@ def bulk_copy_users():
 
 
 def bulk_copy_votes():
-    exists = list(Vote.objects.values_list("uid", flat=True))
-    exists = filter(lambda uid: uid.isdigit(), exists)
-    source = PostsVote.objects.exclude(id__in=exists)
+    exists = Vote.objects.values_list("uid", flat=True)
+    source = PostsVote.objects.all()
 
     def gen_votes():
+        logger.info("Transferring Votes")
         posts = {post.uid: post for post in Post.objects.all()}
         users = {user.profile.uid: user for user in User.objects.all()}
+        stream = zip(count(1), source)
+        stream = islice(stream, LIMIT)
 
-        for vote in source:
+        elapsed, progress = timer_func()
 
+        for index, vote in stream:
+            progress(index, msg="awards")
             post = posts.get(str(vote.post_id))
             author = users.get(str(vote.author_id))
-            # Skip if post or author do not exist
-            if not (post and author):
+
+            # Skip existing votes and incomplete post/author information.
+            if (not (post and author)) or vote.id in exists:
                 continue
 
             vote = Vote(post=post, author=author, type=vote.type, uid=vote.id,
                         date=vote.date)
             yield vote
-
+    # Bulk create the users, then profile.
+    elapsed, progress = timer_func()
     Vote.objects.bulk_create(objs=gen_votes(), batch_size=1000)
-
-    logger.info("Copied all votes from biostar2")
+    vcount = Vote.objects.all().count()
+    elapsed(f"transferred {vcount} users")
 
 
 def bulk_copy_posts():
@@ -155,28 +161,26 @@ def bulk_copy_posts():
     def gen_posts():
         logger.info("transferring posts")
 
-        exists = list(Post.objects.values_list("uid", flat=True))
-        exists = [int(post_uid) for post_uid in exists if post_uid.isdigit()]
-
-        posts = PostsPost.objects.exclude(id__in=exists)
+        exists = Post.objects.values_list("uid", flat=True)
+        posts = PostsPost.objects.order_by("-lastedit_date")
         users = {user.profile.uid: user for user in all_users}
 
         elapsed, progress = timer_func()
-
-        stream = islice(zip(count(1), posts), LIMIT)
+        stream = zip(count(1), posts)
+        stream = islice(stream, LIMIT)
 
         for index, post in stream:
             progress(index, msg="posts")
 
             author = users.get(str(post.author_id))
             lastedit_user = users.get(str(post.lastedit_user_id))
-
-            # Incomplete author information loaded.
-            if not author or lastedit_user:
+            uid = str(post.id)
+            # Incomplete author information loaded or existing posts.
+            if (not (author or lastedit_user)) or uid in exists:
                 continue
 
             content = util.strip_tags(post.content)
-            new_post = Post(uid=post.id, html=post.html, type=post.type,
+            new_post = Post(uid=uid, html=post.html, type=post.type,
                             lastedit_user=lastedit_user, thread_votecount=post.thread_score,
                             author=author, status=post.status, rank=post.rank, has_accepted=post.has_accepted,
                             lastedit_date=post.lastedit_date, book_count=post.book_count, reply_count=post.reply_count,
@@ -189,14 +193,12 @@ def bulk_copy_posts():
             relations[str(new_post.uid)] = [str(post.root_id), str(post.parent_id)]
             yield new_post
 
-        logger.info("Copied all posts from biostar2")
-
     def gen_updates():
         logger.info("Updating post relations")
         posts = {post.uid: post for post in Post.objects.all()}
-        for post_uid in relations:
-            root_uid, parent_uid = relations[post_uid][0], relations[post_uid][1]
-            post = posts[post_uid]
+        for pid in relations:
+            root_uid, parent_uid = relations[pid][0], relations[pid][1]
+            post = posts[pid]
             root = posts.get(root_uid)
             parent = posts.get(parent_uid)
             if not (root and parent):
@@ -205,13 +207,10 @@ def bulk_copy_posts():
             post.parent = parent
             yield post
 
-        logger.info("Updated all posts from biostar2")
-
     def update_threadusers():
         logger.info("Updating thread users.")
         roots = Post.objects.filter(type__in=Post.TOP_LEVEL)
         roots = {root: Post.objects.filter(root=root) for root in roots}
-
         roots = {post: User.objects.filter(pk__in=roots[post].values_list("author")) for post in roots}
 
         for post in roots:
@@ -223,24 +222,34 @@ def bulk_copy_posts():
 
     elapsed, progress = timer_func()
     Post.objects.bulk_create(objs=gen_posts(), batch_size=1000)
-    pcount = User.objects.all().count()
+    pcount = Post.objects.all().count()
     elapsed(f"transferred {pcount} posts")
 
     Post.objects.bulk_update(objs=gen_updates(), fields=["root", "parent"], batch_size=1000)
     update_threadusers()
     elapsed(f"updated {pcount} post threads")
 
+
 def bulk_copy_subs():
     def generate():
         users = {user.profile.uid: user for user in User.objects.all()}
         posts = {post.uid: post for post in Post.objects.all()}
-        exists = list(Subscription.objects.values_list("uid", flat=True))
-        exists = filter(lambda uid: uid.isdigit(), exists)
-        source = PostsSubscription.objects.exclude(id__in=exists)
+        exists = Subscription.objects.values_list("uid", flat=True)
+        source = PostsSubscription.objects.all()
+
         logger.info("Copying subscriptions")
-        for subs in source:
+        elapsed, progress = timer_func()
+        stream = zip(count(1), source)
+        stream = islice(stream, LIMIT)
+
+        for index, subs in stream:
+            progress(index, msg="subscriptions")
             user = users[str(subs.user_id)]
             post = posts[str(subs.post_id)]
+            uid = str(subs.id)
+
+            if uid in exists:
+                continue
             sub = Subscription(uid=subs.id, type=subs.type, user=user, post=post, date=subs.date)
 
             yield sub
@@ -253,10 +262,13 @@ def bulk_copy_subs():
             post.subs_count = posts[post]
             yield post
 
+    elapsed, progress = timer_func()
     Subscription.objects.bulk_create(objs=generate(), batch_size=1000)
-    Post.objects.bulk_update(objs=update_counts(), fields=["subs_count"], batch_size=1000)
+    scount = Subscription.objects.all().count()
+    elapsed(f"transferred {scount} subscriptions")
 
-    logger.info("Load all subscriptions")
+    Post.objects.bulk_update(objs=update_counts(), fields=["subs_count"], batch_size=1000)
+    elapsed(f"Updated {scount} subscription counts")
     return
 
 
@@ -264,7 +276,6 @@ class Command(BaseCommand):
     help = "Migrate users from one database to another."
 
     def add_arguments(self, parser):
-        # Give the database file
         parser.add_argument('--posts', action="store_true", help="Transfer posts from source database to target.")
         parser.add_argument('--users', action="store_true", help="Transfer users from source database to target.")
         parser.add_argument('--votes', action="store_true", help="Transfer votes from source database to target.")
