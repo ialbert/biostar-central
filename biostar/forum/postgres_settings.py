c4ebf0b0f118d5ad85d160fe0dea8e043b9ffac1
Natay Aberra <natay.aberra@gmail.com>
2019-05-01 15:32:12 -0400

    Update by natay on Wed May  1 15:32:12 EDT 2019 from Admins-MacBook-Air.local

--- biostar/forum/postgres_settings.py
+++ biostar/forum/postgres_settings.py
@@ -5,6 +5,8 @@ DEBUG = True
 ONLY_FORUM_URLS = True
 ENGINE_AS_ROOT = False
 
+MENU_BAR = "widgets/forum_menubar.html"
+
 WSGI_APPLICATION = 'conf.postgres.postgres_wsgi.application'
 
 DATABASES = {
