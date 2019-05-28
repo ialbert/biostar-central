3899c3566be3dcd34ebf30700afa43af2b6f067d
Natay Aberra <natay.aberra@gmail.com>
2019-05-28 14:47:05 -0400

    Update by natay on Tue May 28 14:47:05 EDT 2019 from Admins-MacBook-Air.local

--- biostar/forum/middleware.py
+++ biostar/forum/middleware.py
@@ -5,11 +5,9 @@ from django.contrib.auth import logout
 from django.conf import settings
 
 from biostar.accounts.models import Profile
-import biostar.accounts.auth as accounts_auth
-from biostar.message.models import Message
 from .util import now
 from . import tasks, auth
-from .models import Post, Vote
+from .models import Post, Vote, Message
 
 
 logger = logging.getLevelName("biostar")
