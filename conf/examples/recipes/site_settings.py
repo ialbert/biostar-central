dfaa546ce6402109432f8a56557f2bdaa98759f7
Istvan Albert <istvan.albert@gmail.com>
2019-06-05 09:41:06 -0400

    Update by ialbert on Wed Jun  5 09:41:06 EDT 2019 from E1-053620.science.psu.edu

--- conf/examples/recipes/site_settings.py
+++ conf/examples/recipes/site_settings.py
@@ -1,3 +1,5 @@
+from biostar.settings import *
+
 import logging
 
 logger = logging.getLogger("biostar")
@@ -5,20 +7,19 @@ logger = logging.getLogger("biostar")
 DEBUG = False
 
 SITE_ID = 1
-SITE_DOMAIN = "www.lvh.me"
-SITE_NAME = "Biostar Engine"
+SITE_DOMAIN = "www.bioinformatics.recipes"
+SITE_NAME = "Bioinformatics Recipes"
 
 HTTP_PORT = ''
-PROTOCOL = 'http'
+PROTOCOL = 'https'
 
 ALLOWED_HOSTS = [SITE_DOMAIN]
 
-WSGI_APPLICATION = 'conf.site.site_wsgi.application'
+WSGI_APPLICATION = 'conf.run.site_wsgi.application'
 
 try:
     # Attempts to load site secrets.
     from .site_secrets import *
-
     logger.info("Imported settings from .site_secrets")
 except ImportError as exc:
     logger.warn(f"No secrets module could be imported: {exc}")
