15e12d3132a24ed718286328b2b96eb25df7f416
Istvan Albert <istvan.albert@gmail.com>
2019-06-13 11:42:39 -0400

    Update by ialbert on Thu Jun 13 11:42:39 EDT 2019 from E1-053620.science.psu.edu

---
+++ conf/examples/pg/test_settings.py
@@ -0,0 +1,27 @@
+from biostar.engine.settings import *
+from biostar.forum.settings import *
+
+INSTALLED_APPS = DEFAULT_APPS + FORUM_APPS + ENGINE_APPS + ACCOUNTS_APPS + EMAILER_APP
+
+DEBUG = True
+
+WSGI_APPLICATION = 'conf.examples.pg.forum_wsgi.application'
+
+DATABASE_NAME = os.environ.setdefault("DATABASE_NAME", "database.db")
+
+DATABASES = {
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DATABASE_NAME,
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    },
+}
+
+try:
+    from .postgres_secrets import *
+except ImportError as exc:
+    print("No postgres_secrets module could be imported")
