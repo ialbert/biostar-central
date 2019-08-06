c71252816c36c891f274cd2a6b0de68df68b9ed3
Natay <natay.aberra@gmail.com>
2019-08-06 08:47:24 -0400

    working on uwsgi

--- themes/bioconductor/settings.py
+++ themes/bioconductor/settings.py
@@ -68,7 +68,7 @@ AWARDS_FEED_COUNT = 5
 REPLIES_FEED_COUNT = 5
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'biostar.wsgi.application'
+WSGI_APPLICATION = "themes.bioconductor.conf.wsgi.application"
 
 # These parameters will be inserted into the database automatically.
 SITE_NAME = "support.bioconductor.org"
