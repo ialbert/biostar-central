4ec0d064e2a718d0ec8b5e6f6778d510d481d097
Natay <natay.aberra@gmail.com>
2020-02-14 08:43:29 -0500

    cleaned up highlighting

--- biostar/recipes/static/codemirror/mode/shell.js
+++ biostar/recipes/static/codemirror/mode/shell.js
@@ -19,13 +19,13 @@ CodeMirror.defineMode('shell', function() {
 
   var commonAtoms = ["true", "false"];
   var commonKeywords = ["if", "then", "do", "else", "elif", "while", "until", "for", "in", "esac", "fi",
-    "fin", "fil", "done", "exit", "set", "unset", "export", "function"];
+    "fin", "fil", "done", "exit", "set", "unset", "export", "function", "seq"];
   var commonCommands = ["ab", "awk", "bash", "beep", "cat", "cc", "cd", "chown", "chmod", "chroot", "clear",
     "cp", "curl", "cut", "diff", "echo", "find", "gawk", "gcc", "get", "git", "grep", "hg", "kill", "killall",
     "ln", "ls", "make", "mkdir", "openssl", "mv", "nc", "nl", "node", "npm", "ping", "ps", "restart", "rm",
     "rmdir", "sed", "service", "sh", "shopt", "shred", "source", "sort", "sleep", "ssh", "start", "stop",
     "su", "sudo", "svn", "tee", "telnet", "top", "touch", "vi", "vim", "wall", "wc", "wget", "who", "write",
-    "yes", "zsh", "seq", "bwa", "python", "Rscript", "fastq"];
+    "yes", "zsh",  "python", "Rscript",  "fastq", "bwa", "parallel"];
 
   CodeMirror.registerHelper("hintWords", "shell", commonAtoms.concat(commonKeywords, commonCommands));
 
