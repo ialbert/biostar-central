d70994c169aaf49ac12f8e829efed711d0a495a5
Natay Aberra <natay.aberra@gmail.com>
2020-03-01 20:31:13 -0500

    merge

--- biostar/settings.py
+++ biostar/settings.py
@@ -12,6 +12,9 @@ def join(*args):
     return os.path.abspath(os.path.join(*args))
 
 
+# Run tasks in multi threaded mode when in development mode
+MULTI_THREAD = True
+
 # Set the home page to the engine or forum
 INTERNAL_IPS = ['127.0.0.1']
 
