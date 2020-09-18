cd7df17cf891632d6ae9a516e9911e82013354b9
Natay Aberra <natay.aberra@gmail.com>
2020-09-18 07:52:54 -0400

    added token field to profiles

--- biostar/recipes/api.py
+++ biostar/recipes/api.py
@@ -210,3 +210,60 @@ def recipe_template(request, uid):
 
     return HttpResponse(content=payload, content_type="text/plain")
 
+
+def validate_data_api(target, request, source, user, data):
+    # Maximum size for a data to be updated via api.
+    # Get the file to update
+    fname = request.data.get("fname")
+
+    # Get token to validate user.
+    token = request.data.get("token")
+
+    maxsize = 50
+
+    return False, ''
+
+
+@api_error_wrapper(['GET', 'PUT'])
+@require_api_key(type=Data)
+@ratelimit(key='ip', rate='20/m')
+def update_data(request, uid):
+    """
+    GET request: Returns data
+    PUT request: Updates file in data with given file.
+    """
+
+    data = Data.objects.filter(uid=uid).first()
+
+    # Get token to validate user access.
+    token = request.data.get("token")
+    target_user = User.objects.filter(profile__token=token).first()
+
+    # Get the source that will replace target
+    source = request.data.get("file", "")
+
+    fname = source.name
+
+    print(source, "LOPPPLEVOP")
+    1 / 0
+
+    # Search for target in the data directory.
+    target = list(filter(lambda f: f.endswith(fname), data.get_files()))[0]
+
+    # Get the source that will replace target
+    source = request.data.get("file", "")
+
+    # Validate both files before processing.
+    valid, msg = validate_data_api(target=target, request=request, user=target_user)
+
+    if not valid:
+        return HttpResponse(content=msg, content_type="text/plain")
+
+    # Write source into target
+    if request.method == "PUT":
+        target = util.write_stream(stream=source, dest=target)
+
+    # Return file contents in payload
+    payload = open(target, 'r').read()
+
+    return HttpResponse(content=payload, content_type="text/plain")
