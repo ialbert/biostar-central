e35f85f4baf78eff6f272b6fc621bdce80d1eca8
Natay Aberra <natay.aberra@gmail.com>
2020-11-11 08:26:33 -0500

    clean up

--- biostar/emailer/sender.py
+++ biostar/emailer/sender.py
@@ -127,7 +127,11 @@ def send_mass_html_mail(subject, message, message_html, from_email, recipient_li
     connection = get_connection(fail_silently=False)
 
     def make_email(rec):
-        msg = EmailMultiAlternatives(subject, message, from_email, rec).attach_alternative(message_html,
+        msg = EmailMultiAlternatives(subject=subject,
+                                     body=message,
+                                     from_email=from_email,
+                                     to=rec,
+                                     connection=connection).attach_alternative(message_html,
                                                                          "html/text")
         return msg
 
