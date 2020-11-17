a520a9189322e3f18b8e191e53b3923d3b18babf
Natay Aberra <natay.aberra@gmail.com>
2020-11-17 10:44:12 -0500

    fix up in biocon theme

--- biostar/accounts/util.py
+++ biostar/accounts/util.py
@@ -1,11 +1,11 @@
 import uuid
 from datetime import datetime
-
+from django.utils import timezone
 from django.utils.timezone import utc
 
 def get_uuid(limit=32):
     return str(uuid.uuid4())[:limit]
 
 def now():
-    return datetime.utcnow().replace(tzinfo=utc)
+    return timezone.now()
 
