ed98608dd0d96fb00f4ac01a3c92f449c4fbbaf3
Natay Aberra <natay.aberra@gmail.com>
2020-11-17 12:41:34 -0500

    fix up themes

--- themes/bioconductor/static/theme.js
+++ themes/bioconductor/static/theme.js
@@ -1,9 +1,27 @@
 
 
+
 $(document).ready(function () {
+    function cancel_answers() {
+        $('.answer-text').hide();
+        $('.answer-text textarea').val('')
+    }
+
     $('.show-answer').click(function () {
         $(".answer-text").show();
     });
 
     remove_trigger();
+
+        $('body').keyup(function (event) {
+            if (event.keyCode === 27) {
+                // Cancel answer text area.
+                cancel_answers()
+            }
+        });
+
+        $('.answer-text .cancel').click(function () {
+            cancel_answers()
+        });
+
 });
