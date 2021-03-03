c2da3f728654a1e94015e13b9e1118a2c1c42e37
Natay Aberra <natay.aberra@gmail.com>
2021-03-03 09:09:25 -0500

    update

--- biostar/settings.py
+++ biostar/settings.py
@@ -98,7 +98,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'ratelimit.middleware.RatelimitMiddleware',
 
 ]
 
