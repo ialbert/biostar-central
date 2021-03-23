8e00cc0ca0af9ab0edc07918f43dacd18e511572
Natay Aberra <natay.aberra@gmail.com>
2021-03-23 09:26:37 -0400

    update

--- biostar/forum/scripts/bump.sh
+++ biostar/forum/scripts/bump.sh
@@ -20,11 +20,5 @@ USER=www
 # Set the configuration module.
 export DJANGO_SETTINGS_MODULE=conf.run.site_settings
 
-# Backup location
-mkdir -p export/backup
-
-# pg_dump the database
-python manage.py tasks --action pg_dump --outdir export/backup  --user ${USER} --hourly
-
 # bump a random post
-#python manage.py tasks --action bump
+python manage.py tasks --action bump
