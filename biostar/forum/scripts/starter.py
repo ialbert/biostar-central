15aab4205fbcda0ca036ab04304a57ad515013ca
Istvan Albert <istvan.albert@gmail.com>
2021-03-24 11:00:12 -0400

    small style fix

--- biostar/forum/scripts/starter.py
+++ biostar/forum/scripts/starter.py
@@ -1,33 +1,39 @@
 # Starter script template for writing programs that
 # interact with the django site
-import sys, os
+import os
+import sys
 
 # Biostar home directory
-DIR="/home/ialbert/app/biostar-central"
+PATH = "~/app/biostar-central"
 
-sys.path.append(DIR)
+PATH = os.path.expanduser(PATH)
+
+sys.path.append(PATH)
 
 import django
+
 from django.conf import settings
 
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biostar.forum.settings")
 
 # Import the settings file
 from biostar.forum import settings as app_settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "biostar.forum.settings")
+#print (os.environ.get('DJANGO_SETTINGS_MODULE', "*** NOT FOUND ***") )
 
 # Apply settings onto the django settings module.
-#settings.configure(default_settings=app_settings, DEBUG=True, LOGGING_CONFIG=None)
-
+settings.configure(default_settings=app_settings, DEBUG=True, LOGGING_CONFIG=None, FORCE_SCRIPT_NAME=None)
 
 # Bootstrap the Django framework.
 django.setup()
 
 # You can now import and user models in the app.
-from biostar.forum.models import User, Post
+from biostar.forum.models import User
+
+
 
 def main():
-    users = User.objects.filter().all()
+    users = User.objects.all().count()
     print(users)
     pass
 
