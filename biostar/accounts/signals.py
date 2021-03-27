1686f943cc9c8e1c58d0fe5b304f7740788af018
Istvan Albert <istvan.albert@gmail.com>
2021-03-27 00:04:47 -0400

    add spam cleaner

--- biostar/accounts/signals.py
+++ biostar/accounts/signals.py
@@ -3,32 +3,39 @@ from django.dispatch import receiver
 
 from biostar.accounts.models import Profile, User
 from biostar.accounts import util, tasks
-from django.template.defaultfilters import slugify
+
+
+@receiver(pre_save, sender=User)
+def create_uuid(sender, instance, *args, **kwargs):
+    # Generate a unique username if it does not exist.
+    instance.username = instance.username or util.get_uuid(8)
 
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, raw, using, **kwargs):
 
     if created:
-        # Set the username to a simpler form.
-        username = f"{instance.first_name}-{instance.pk}" if instance.first_name else f'user-{instance.pk}'
-        username = slugify(username)
-        if User.objects.filter(username=username).exclude(id=instance.pk).exists():
-            username = util.get_uuid(6)
+        # Set the username a simpler username.
+        username = f"{instance.pk}"
 
+        # Update the user with a simpler username.
         User.objects.filter(pk=instance.pk).update(username=username)
 
         # Make sure staff users are also moderators.
-        role = Profile.MANAGER if (instance.is_staff or instance.is_superuser) else Profile.READER
-        Profile.objects.using(using).create(user=instance, uid=username, name=instance.first_name, role=role)
+        role = (
+            Profile.MANAGER
+            if (instance.is_staff or instance.is_superuser)
+            else Profile.READER
+        )
+
+        # Create a user profile associate with the user.
+        Profile.objects.using(using).create(
+            user=instance, uid=username, name=instance.first_name, role=role
+        )
+
+        # Send the welcome message.
         user_ids = [instance.pk]
         tasks.create_messages(user_ids=user_ids, template="messages/welcome.md")
 
     # Recompute watched tags
     instance.profile.add_watched()
-
-
-@receiver(pre_save, sender=User)
-def create_uuid(sender, instance, *args, **kwargs):
-
-    instance.username = instance.username or util.get_uuid(8)
