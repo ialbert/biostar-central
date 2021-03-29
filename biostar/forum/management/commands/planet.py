82cac431e934be15122106e72106dab5248f06bd
Natay Aberra <natay.aberra@gmail.com>
2021-03-29 11:27:10 -0400

    update

--- biostar/forum/management/commands/planet.py
+++ biostar/forum/management/commands/planet.py
@@ -1,6 +1,6 @@
 
 from django.conf import settings
-
+from django.db.models import Max, Count
 from biostar.forum.util import now
 from biostar.planet.models import Blog, BlogPost
 from django.core.management.base import BaseCommand
@@ -27,6 +27,23 @@ def fake():
 
     BlogPost.objects.create(blog=blog, title='Creating a fake blog post.', creation_date=now())
 
+
+def delete_repeats():
+
+    blogs = Blog.objects.annotate(updated_date=Max("blogpost__creation_date"))
+    blogs = blogs.annotate(count=Count("blogpost__id"))
+    # Order by most blog posts.
+    blogs = blogs.order_by("-count")
+
+    seen = set()
+    for blg in blogs:
+        # Delete the blog if already seen
+        if blg.feed in seen:
+            blg.delete()
+            logger.debug(f"deleted {blg.feed}")
+        seen.update([blg.feed])
+
+
 class Command(BaseCommand):
     help = 'Create search index for the forum app.'
 
@@ -38,6 +55,8 @@ class Command(BaseCommand):
         parser.add_argument('--report', action='store_true', default=False, help="Reports on the content of the index.")
         parser.add_argument('--update', dest='update', default=0, type=int, help='updates existing blogs with latest feeds')
         parser.add_argument('--fake', dest='fake',  action="store_true", default=False, help='Create fake blog entries.')
+        parser.add_argument('--delete_repeats', dest='delete',  action="store_true", default=False,
+                            help='Delete repeated blogs in database.')
 
     def handle(self, *args, **options):
         # Create the planet directory if it is missing
@@ -47,13 +66,15 @@ class Command(BaseCommand):
             fake()
 
         fname = options['add']
+
+        if options['delete']:
+            delete_repeats()
+
         if fname:
             fname = os.path.abspath(os.path.join(settings.PLANET_DIR, fname))
             auth.add_blogs(fname)
 
         if options['download']:
-            fname = os.path.abspath(os.path.join(settings.PLANET_DIR, 'example-feeds.txt'))
-            auth.add_blogs(fname)
             auth.download_blogs()
 
         count = options['update']
--- biostar/planet/views.py
+++ biostar/planet/views.py
@@ -12,8 +12,22 @@ def blog_list(request):
     page = request.GET.get("page", 1)
     blogposts = BlogPost.objects.select_related("blog").order_by("-creation_date")
 
-    blogs = Blog.objects.all().annotate(updated_date=Max("blogpost__creation_date"),
-                                        count=Count("blogpost__id")).order_by("-updated_date", "-list_order")
+    blogs = Blog.objects.annotate(updated_date=Max("blogpost__creation_date"))
+    blogs = blogs.annotate(count=Count("blogpost__id"))
+    blogs = blogs.order_by("-updated_date", "-list_order")[:100]
+
+    # .distinct() on textfield not allowed on sql database backend.
+    # .distinct() + .annotate() not implemented in postgres unless ordered by text field.
+    # seen = set()
+    #
+    # def distinct(blg):
+    #     # Return True if feed is already seen
+    #     if blg.feed in seen:
+    #         return False
+    #     seen.update([blg.feed])
+    #     return True
+    #
+    # blogs = [b for b in blogs if distinct(b)]
 
     blogposts = Paginator(blogposts, per_page=settings.BLOGS_PER_PAGE)
     blogposts = blogposts.get_page(page)
