6862a38996b10e5dcca58172a4518e6cb35340cb
Natay Aberra <natay.aberra@gmail.com>
2021-04-07 11:47:31 -0400

    remove reset for anon users

--- biostar/planet/views.py
+++ biostar/planet/views.py
@@ -22,17 +22,11 @@ def now():
 
 def reset_planet_counts(request):
 
-    # Set the session.
-    counts = dict(planet_count=0)
-    request.session[settings.SESSION_COUNT_KEY] = counts
+    if request.user.is_authenticated:
 
-    if request.user.is_anonymous:
-        # Get the ip
-        ip = get_ip(request)
-        # Expire counts cache in 24 hours
-        expire = 3600 * 24
-        # Set the counts to zero in the cache
-        cache.set(ip, 0, expire)
+        # Set the session.
+        counts = dict(planet_count=0)
+        request.session[settings.SESSION_COUNT_KEY] = counts
 
 
 def set_planet_count(request):
