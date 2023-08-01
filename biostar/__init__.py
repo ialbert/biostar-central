3ce389113ead1873edec2f1caf0790f80d1e7a63
Istvan Albert <istvan.albert@gmail.com>
2023-08-01 15:41:16 -0400

    updated django to 4.2

--- biostar/__init__.py
+++ biostar/__init__.py
@@ -5,3 +5,13 @@ except ImportError as exc:
     pass
 
 VERSION = '2.3.6'
+
+# Monkeypatch the translation module to avoid errors in Django 4.2
+import django.utils.translation
+
+django.utils.translation.ugettext_lazy = django.utils.translation.gettext_lazy
+
+import django.utils.encoding
+
+django.utils.encoding.force_text = django.utils.encoding.force_str
+django.utils.encoding.smart_text = django.utils.encoding.smart_str
